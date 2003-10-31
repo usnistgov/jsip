30668e5981443108a4dd3ed3d146a60189227f5a
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2003-10-31 14:14:42 +0000

    Reviewed by:   M. Ranganathan
    Fix parser to allow both \r\n and \n for end of line separator

--- trunk/src/gov/nist/javax/sdp/parser/SDPAnnounceParser.java
+++ trunk/src/gov/nist/javax/sdp/parser/SDPAnnounceParser.java
@@ -4,8 +4,10 @@ import gov.nist.core.*;
 import gov.nist.javax.sdp.fields.*;
 import gov.nist.javax.sdp.*;
 import java.text.ParseException;
-/**
+/** Parser for SDP Announce messages.
  *
+ * Acknowledgement: this includes a bug fix submitted by 
+ * Rafael Barriuso rbarriuso@dit.upm.es
  */
 public class SDPAnnounceParser extends ParserCore {
 
@@ -21,38 +23,43 @@ public class SDPAnnounceParser extends ParserCore {
 	
     }
 
-	/** Create a new SDPAnnounceParser.
-	*@param sdpMessage message containing the sdp announce message.
-	*/
+
+    /** Create a new SDPAnnounceParser.
+    *@param sdpMessage message containing the sdp announce message.
+    *
+    */
      public SDPAnnounceParser(String sdpAnnounce) {
-          sdpMessage = new Vector();
-	  int start = 0;
-	  sdpMessage = new Vector();
-	  // Bug fix by Andreas Bystrom.
-	  while (start < sdpAnnounce.length() ) {
-  	      int add = 0;
-  	      int index = sdpAnnounce.indexOf("\n",start);
-  	      if (index == -1) break;
-  	      if (sdpAnnounce.charAt(index - 1) == '\r') {
-    		index = index - 1;
-    		add = 1;
-  	      }
-   	     String line = sdpAnnounce.substring(start,index);
-  	     start = index + 1 + add;
-  	    sdpMessage.addElement(line);
-	  }
-	  
-/**
-	  while (start < sdpAnnounce.length() ) {
-	     int index = sdpAnnounce.indexOf("\r\n",start);
-	     if (index == -1) break;
-	     String line = sdpAnnounce.substring(start,index);
-	     start = index + 2;
-	     sdpMessage.addElement(line);
-	  }
-**/
-	
+      int start = 0;String line = null;
+      sdpMessage = new Vector();
+      // Bug fix by Andreas Bystrom.
+      while (start < sdpAnnounce.length()) {
+            int add = 0;
+            int index = sdpAnnounce.indexOf("\n",start);
+            int index2= sdpAnnounce.indexOf("\r",start);
+
+            if(index>0 && index2<0){ 
+		// there are only "\n" separators
+               line = sdpAnnounce.substring(start,index);
+               start=index+1;
+            }else if(index<0 && index2>0){ 
+			//bug fix: there are only "\r" separators
+              line = sdpAnnounce.substring(start,index2);
+              start=index2+1;
+            }else if(index>0 && index2>0){ 
+		// there are "\r\n" or "\n\r" (if exists) separators
+                if(index>index2){
+                  line = sdpAnnounce.substring(start,index2);
+                  start=index+1;
+                }else{
+                  line = sdpAnnounce.substring(start,index);
+                  start=index2+1;
+                }
+            }else if(index<0 && index2<0) // end
+                break;
+          sdpMessage.addElement(line);
+      }
      }
+
 
      public SessionDescriptionImpl parse()  throws ParseException {
 	SessionDescriptionImpl retval = new SessionDescriptionImpl();
