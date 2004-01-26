e065d9ca1fe34f1a707538ef9d15bd448019fecd
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2004-01-26 19:12:49 +0000

    Reviewed by:   mranga moved SIMULATION tag to first columnm

--- trunk/src/gov/nist/javax/sip/NistSipMessageHandlerImpl.java
+++ trunk/src/gov/nist/javax/sip/NistSipMessageHandlerImpl.java
@@ -19,7 +19,7 @@ import gov.nist.core.*;
  * NIST-SIP stack and event model with the JAIN-SIP stack. Implementors
  * of JAIN services need not concern themselves with this class.
  *
- * @version JAIN-SIP-1.1 $Revision: 1.7 $ $Date: 2004-01-22 13:26:28 $
+ * @version JAIN-SIP-1.1 $Revision: 1.8 $ $Date: 2004-01-26 19:12:49 $
  *
  * @author M. Ranganathan <mranga@nist.gov>  <br/>
  * Bug fix Contributions by Lamine Brahimi and  Andreas Bystrom. <br/>
@@ -69,11 +69,11 @@ public class NistSipMessageHandlerImpl
 
 		SIPTransaction transaction = transactionChannel;
 		// Look for the registered SIPListener for the message channel.
-		//ifndef SIMULATION
-		//
+//ifndef SIMULATION
+//
 		synchronized (sipProvider) {
-			//endif
-			//
+//endif
+//
 			if (sipRequest.getMethod().equalsIgnoreCase(Request.ACK)) {
 				// Could not find transaction. Generate an event
 				// with a null transaction identifier.
@@ -295,11 +295,11 @@ public class NistSipMessageHandlerImpl
 							(Request) sipRequest);
 			}
 			sipProvider.handleEvent(sipEvent, transaction);
-			//ifndef SIMULATION
-			//
+//ifndef SIMULATION
+//
 		}
-		//endif
-		//
+//endif
+//
 
 	}
 
@@ -442,4 +442,28 @@ public class NistSipMessageHandlerImpl
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.7  2004/01/22 13:26:28  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
