4d8e73816bb2012150b2350a9b7022352e3882f1
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2004-01-27 13:52:11 +0000

    Reviewed by:   mranga Fixed server/user-agent parser. suppress sending ack to TU when retransFilter is enabled and ack is retransmitted.

--- trunk/src/gov/nist/javax/sip/parser/UserAgentParser.java
+++ trunk/src/gov/nist/javax/sip/parser/UserAgentParser.java
@@ -6,7 +6,7 @@ import java.text.ParseException;
 /**
  * Parser for UserAgent header.
  *
- * @version JAIN-SIP-1.1 $Revision: 1.5 $ $Date: 2004-01-22 13:26:32 $
+ * @version JAIN-SIP-1.1 $Revision: 1.6 $ $Date: 2004-01-27 13:52:11 $
  *
  * @author Olivier Deruelle <deruelle@nist.gov>  <br/>
  * @author M. Ranganathan <mranga@nist.gov>  <br/>
@@ -52,13 +52,16 @@ public class UserAgentParser extends HeaderParser {
 					String comment = this.lexer.comment();
 					userAgent.addProductToken('(' + comment + ')');
 				} else {
+					int marker = 0;
 					String tok;
 					try {
+						marker = this.lexer.markInputPosition();
 						tok = this.lexer.getString('/');
 						if (tok.charAt(tok.length() - 1) == '\n')
 							tok = tok.trim();
 						userAgent.addProductToken(tok);
 					} catch (ParseException ex) {
+						this.lexer.rewindInputPosition(marker);
 						tok = this.lexer.getRest();
 						userAgent.addProductToken(tok);
 						break;
@@ -94,4 +97,28 @@ public class UserAgentParser extends HeaderParser {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.5  2004/01/22 13:26:32  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
