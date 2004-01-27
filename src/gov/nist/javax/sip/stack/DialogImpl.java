c8e6aee58afc59c452708dfa94e6c5faac55e76b
M. Ranganathan <mranga@nist.gov>
2004-01-27 13:52:11 +0000

    Reviewed by:   mranga Fixed server/user-agent parser. suppress sending ack to TU when retransFilter is enabled and ack is retransmitted.

--- src/gov/nist/javax/sip/stack/DialogImpl.java
+++ src/gov/nist/javax/sip/stack/DialogImpl.java
@@ -24,7 +24,7 @@ import java.text.ParseException;
  * retrieve this structure from the SipStack. Bugs against route set 
  * management were reported by Antonis Karydas and Brad Templeton.
  *
- *@version  JAIN-SIP-1.1 $Revision: 1.13 $ $Date: 2004-01-22 13:26:33 $
+ *@version  JAIN-SIP-1.1 $Revision: 1.14 $ $Date: 2004-01-27 13:52:11 $
  *
  *@author M. Ranganathan <mranga@nist.gov>  <br/>
  *
@@ -52,6 +52,7 @@ public class DialogImpl implements javax.sip.Dialog {
 	private int dialogState;
 	protected boolean ackSeen;
 	protected SIPRequest lastAck;
+	protected boolean ackProcessed;
 
 	private int retransmissionTicksLeft;
 	private int prevRetransmissionTicks;
@@ -1412,4 +1413,28 @@ public class DialogImpl implements javax.sip.Dialog {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.13  2004/01/22 13:26:33  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
