c032810052e62f4e0e3b8e478709af20e489ae3d
M. Ranganathan <mranga@nist.gov>
2004-02-13 19:33:18 +0000

    Added torture tests.

---
+++ src/test/torture/TokenValues.java
@@ -0,0 +1,7 @@
+package test.torture;
+public interface TokenValues {
+	public final static String EQUALS = "=";
+	public final static String SLASH = "/";
+	public final static String LESS_THAN = "<";
+	public final static String GREATER_THAN = ">";
+}
