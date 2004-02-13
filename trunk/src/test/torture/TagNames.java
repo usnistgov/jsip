478a414343b9e48cbd8a6c2d3f5e4ea7bfbef6b2
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2004-02-13 19:33:18 +0000

    Added torture tests.

---
+++ trunk/src/test/torture/TagNames.java
@@ -0,0 +1,30 @@
+/******************************************************
+ * File: TagNames.java
+ * created 06-Dec-00 8:05:39 PM by mranga
+ * Tag names and attributes.
+ */
+
+package test.torture;
+
+public interface TagNames 
+{
+ 	public final static String TORTURE = "torture";
+	public final static String TESTCASE = "testcase";
+	public final static String EXPECT_EXCEPTION = 
+					"expect-exception-callback";
+	public final static String MESSAGE = "message";
+	public final static String SIP_URL = "sip-url";
+	public final static String DESCRIPTION = "description";
+	public final static String EXCEPTION_CLASS = "exception-class";
+	public final static String EXCEPTION_MESSAGE = "exception-message";
+	public final static String EXCEPTION_TEXT = "exception-text";
+	public final static String PARSED_OUTPUT = "parsed-output";
+	public final static String DIAGNOSTIC = "diagnostic";
+	public final static String DIAGNOSTIC_MESSAGES = "diagnostic-messages";
+	public final static String REASON = "reason";
+	public final static String STACK_TRACE = "stack-trace";
+	public final static String TEST_OUTPUT = "test-output";
+	public final static String STATUS = "status";
+	public final static String SIP_HEADER = "sip-header";
+	public final static String OUTPUT_RESULTS = "output-results";
+}
