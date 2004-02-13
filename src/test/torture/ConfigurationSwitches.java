3504e840ea59d4e56e7b6d390198e9c6ce80bff6
M. Ranganathan <mranga@nist.gov>
2004-02-13 19:33:18 +0000

    Added torture tests.

---
+++ src/test/torture/ConfigurationSwitches.java
@@ -0,0 +1,12 @@
+package test.torture;
+
+interface ConfigurationSwitches {
+	public final static String OUTPUT_PARSED_STRUCTURE =
+			"output-parsed-structures";
+	public final static String DEBUG = "debug";
+	public final static String TRUE = "true";
+	public final static String TESTLOG = "testlog";
+	public final static String ABORT_ON_FAIL = "abort-on-fail";
+	public final static String TEST_JAIN = "test-jain";
+	public final static String JAIN_TESTLOG = "jaintestlog";
+}
