80badf7ebd24e16f1d5f21efdb2b1c03fff179e7
M. Ranganathan <mranga@nist.gov>
2004-03-18 22:01:20 +0000

    Reviewed by:   mranga Get rid of the PipedInputStream from pipelined parser to avoid a copy.

---
+++ src/gov/nist/javax/sip/parser/Pipeline.java
@@ -0,0 +1,91 @@
+package gov.nist.javax.sip.parser;
+import  java.io.*;
+import java.util.*;
+
+/** Input class for the pipelined parser.
+*/
+
+public class Pipeline extends  InputStream {
+	private LinkedList buffList;
+	private Buffer currentBuffer;
+	private boolean isClosed;
+
+	class Buffer {
+		byte[] bytes;
+		int length ;
+		int ptr;
+		public Buffer (byte[] bytes, int length) {
+			ptr = 0;
+			this.length  = length;
+			this.bytes = bytes;
+		}
+		public int getNextByte() {
+		    int retval =  (int) bytes[ptr ++ ];
+		    return retval;
+		}
+			
+	}
+
+
+
+	public Pipeline () {
+		buffList = new LinkedList();
+	}
+
+	public void write (byte [] bytes, int start, int length) 
+		throws IOException {
+		if (this.isClosed) 
+			throw new IOException ("Closed!!");
+		Buffer buff = new Buffer(bytes,length);
+		buff.ptr = start;
+		synchronized (this.buffList) {
+			buffList.add(buff);
+			buffList.notifyAll();
+		}
+	}
+	public void write (byte [] bytes) throws IOException {
+		if (this.isClosed) 
+			throw new IOException ("Closed!!");
+		Buffer buff = new Buffer(bytes,bytes.length);
+		synchronized (this.buffList) {
+			buffList.add(buff);
+			buffList.notifyAll();
+		}
+	}
+
+	public void close() {
+		this.isClosed = true;
+		synchronized(this.buffList) {
+			this.buffList.notifyAll();
+		}
+	}
+
+	public int read() {
+		if (this.isClosed) return -1;
+		synchronized (this.buffList) {
+		    if (currentBuffer != null && currentBuffer.ptr <
+			currentBuffer.length)  {
+			int retval =   currentBuffer.getNextByte();
+		        if (currentBuffer.ptr == currentBuffer.length )
+				this.currentBuffer = null;
+			return retval;
+		     }
+		     try {
+			if (this.buffList.isEmpty()) this.buffList.wait();
+			if (this.isClosed) return -1;
+			currentBuffer = (Buffer) this.buffList.removeFirst();
+			int retval =   currentBuffer.getNextByte();
+		        if (currentBuffer.ptr == currentBuffer.length )
+				this.currentBuffer = null;
+			return retval;
+		     } catch (InterruptedException ex) {
+			return -1;
+		     } catch (NoSuchElementException ex ) {
+			ex.printStackTrace();
+			return -1;
+		     }
+		}
+	}
+
+
+}
