c1bb41189d2fe36a5d7e4439fdb7e69df953fb4b
M. Ranganathan <mranga@nist.gov>
2004-04-06 12:28:23 +0000

    Reviewed by:   mranga changed locale to Locale.getDefault().getCountry() moved check for valid transaction state up in the stack so unfruitful responses are pruned early.

--- src/gov/nist/javax/sip/header/ContentLanguage.java
+++ src/gov/nist/javax/sip/header/ContentLanguage.java
@@ -50,7 +50,7 @@ import java.util.Locale;
 *   Content-Language MAY be applied to any media type -- it is not
 *   limited to textual documents.
 *</pre>
-* @version JAIN-SIP-1.1 $Revision: 1.3 $ $Date: 2004-01-22 13:26:29 $
+* @version JAIN-SIP-1.1 $Revision: 1.4 $ $Date: 2004-04-06 12:28:23 $
 */
 public class ContentLanguage
 	extends SIPHeader
@@ -70,7 +70,8 @@ public class ContentLanguage
 	 */
 	public ContentLanguage(String languageTag) {
 		super(CONTENT_LANGUAGE);
-		this.locale = new Locale(languageTag, Locale.US.getCountry());
+		this.locale = new Locale(languageTag,
+				Locale.getDefault().getCountry());
 	}
 
 	/**
@@ -92,7 +93,8 @@ public class ContentLanguage
 	 * @param languageTag -- language tag to set.
 	 */
 	public void setLanguageTag(String languageTag) {
-		this.locale = new Locale(languageTag, Locale.US.getCountry());
+		this.locale = new Locale(languageTag,
+				Locale.getDefault().getCountry());
 	}
 
 	/**
@@ -121,4 +123,28 @@ public class ContentLanguage
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2004/01/22 13:26:29  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
