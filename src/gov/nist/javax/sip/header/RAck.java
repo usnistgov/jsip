c84cb5f4b9e42e73ca4fe925f66b3eaf30c0945f
M. Ranganathan <mranga@nist.gov>
2004-04-06 11:11:59 +0000

    Submitted by:  Bruno Konik Reviewed by:   mranga Encoding in the wrong order (rseq first and then cseq - was inverted)

--- src/gov/nist/javax/sip/header/RAck.java
+++ src/gov/nist/javax/sip/header/RAck.java
@@ -8,7 +8,7 @@ import java.text.ParseException;
 /**
  * RAck SIP Header implementation
  *
- * @version JAIN-SIP-1.1 $Revision: 1.2 $ $Date: 2004-01-22 13:26:29 $
+ * @version JAIN-SIP-1.1 $Revision: 1.3 $ $Date: 2004-04-06 11:11:59 $
  *
  * @author M. Ranganathan <mranga@nist.gov>  <br/>
  *
@@ -30,12 +30,15 @@ public class RAck extends SIPHeader implements javax.sip.header.RAckHeader {
 	/**
 	 * Encode the body of this header (the stuff that follows headerName).
 	 * A.K.A headerValue.
+	 * 
 	 */
 	protected String encodeBody() {
+		// Bug reported by Bruno Konik - was encoded in
+		// the wrong order.
 		return new StringBuffer()
-			.append(cSeqNumber)
-			.append(SP)
 			.append(rSeqNumber)
+			.append(SP)
+			.append(cSeqNumber)
 			.append(SP)
 			.append(method)
 			.toString();
@@ -111,4 +114,28 @@ public class RAck extends SIPHeader implements javax.sip.header.RAckHeader {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/01/22 13:26:29  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
