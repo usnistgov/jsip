6279cfb322670cb7715897e4159fe1615508e8c8
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2004-04-20 16:37:40 +0000

    Submitted by:  Thomas Froment Reviewed by:  mranga Fixes a parsing corner case.

--- trunk/src/gov/nist/core/HostNameParser.java
+++ trunk/src/gov/nist/core/HostNameParser.java
@@ -137,6 +137,7 @@ public class HostNameParser extends ParserCore {
 			// Has a port?
 			if (lexer.hasMoreChars() && lexer.lookAhead(0) == ':') {
 				lexer.consume(1);
+				lexer.SPorHT(); // white space before port number should be accepted (CERT test)
 				try {
 					String port = lexer.number();
 					hp.setPort(Integer.parseInt(port));
@@ -160,7 +161,10 @@ public class HostNameParser extends ParserCore {
 				"foo.bar.com:1234",
 				"proxima.chaplin.bt.co.uk",
 				"129.6.55.181:2345",
-				":2345" };
+				":1234",
+				"foo.bar.com:         1234"
+			};
+			
 		for (int i = 0; i < hostNames.length; i++) {
 			try {
 				HostNameParser hnp = new HostNameParser(hostNames[i]);
@@ -176,4 +180,28 @@ public class HostNameParser extends ParserCore {
 }
 /*
  * $Log: not supported by cvs2svn $
+  * Revision 1.4  2004/01/22 13:26:27  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
