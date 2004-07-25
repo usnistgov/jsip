7478b3b6245993e3b9654e46dcb1a34e54d84518
M. Ranganathan <mranga@nist.gov>
2004-07-25 19:26:44 +0000

    Reviewed by:   mranga Allows multiple Authorization headers in a message. Some minor cleanup.

--- src/gov/nist/javax/sip/message/ListMap.java
+++ src/gov/nist/javax/sip/message/ListMap.java
@@ -8,7 +8,7 @@ import java.util.Hashtable;
 
 /**
  * A map of which of the standard headers may appear as a list 
- * @version JAIN-SIP-1.1 $Revision: 1.2 $ $Date: 2004-01-22 13:26:31 $
+ * @version JAIN-SIP-1.1 $Revision: 1.3 $ $Date: 2004-07-25 19:26:44 $
  */
 class ListMap   {
 	private final static String SIPHEADERS_PACKAGE = 
@@ -124,6 +124,12 @@ class ListMap   {
 		GenericObject.getClassFromName
 			(SIPHEADERS_PACKAGE +".ProxyAuthenticateList"));
 
+	    headerListTable.put(
+		GenericObject.getClassFromName
+			(SIPHEADERS_PACKAGE + ".Authorization"),
+		GenericObject.getClassFromName
+			(SIPHEADERS_PACKAGE +".AuthorizationList"));
+
 	   headerListTable.put(
 		GenericObject.getClassFromName
 			(SIPHEADERS_PACKAGE + ".Allow"),
@@ -217,4 +223,28 @@ class ListMap   {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/01/22 13:26:31  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
