044b43763bcdf9fbe904b97a20b9e8ea64c07531
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2004-08-23 23:56:21 +0000

    Reviewed by:   mranga forgot to set isDaemon in one or two places where threads were being created and cleaned up some minor junk.

--- trunk/src/gov/nist/javax/sip/header/From.java
+++ trunk/src/gov/nist/javax/sip/header/From.java
@@ -10,7 +10,7 @@ import java.text.ParseException;
 /**
  * From SIP Header.
  *
- * @version JAIN-SIP-1.1 $Revision: 1.3 $ $Date: 2004-01-22 13:26:29 $
+ * @version JAIN-SIP-1.1 $Revision: 1.4 $ $Date: 2004-08-23 23:56:20 $
  *
  * @author M. Ranganathan <mranga@nist.gov>  <br/>
  *
@@ -50,8 +50,6 @@ public final class From
 				return false;
 			}
 			return true;
-			// exitpoint = 3;
-			// return parms.equals(otherTo.parms);
 		} finally {
 			// System.out.println("equals " + retval + exitpoint);
 		}
@@ -164,4 +162,28 @@ public final class From
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2004/01/22 13:26:29  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
