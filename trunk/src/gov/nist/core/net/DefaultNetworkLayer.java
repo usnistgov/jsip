5d833d90938aef44c12db71e641c60386cfc5ad6
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2004-08-30 15:22:08 +0000

    Network layer. This allows for low level monitoring and control over the IO objects used by the stack. This is useful for statistics gathering, low level security etc.

---
+++ trunk/src/gov/nist/core/net/DefaultNetworkLayer.java
@@ -0,0 +1,41 @@
+package gov.nist.core.net;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.SocketException;
+
+/**
+ * passes straight through to java platform implementations
+ *
+ * @author m.andrews
+ *
+ */
+public class DefaultNetworkLayer implements NetworkLayer {
+
+    public static final DefaultNetworkLayer SINGLETON = new DefaultNetworkLayer();
+
+    private DefaultNetworkLayer() {
+
+    }
+
+    public ServerSocket createServerSocket(int port, int backlog,
+            InetAddress bindAddress) throws IOException {
+        System.out.println("DefaultNetworkLayer.createServerSocket(" + port + ", " + backlog + ", "
+                + bindAddress + ")");
+        return new ServerSocket(port, backlog, bindAddress);
+    }
+
+    public DatagramSocket createDatagramSocket() throws SocketException {
+        System.out.println("DefaultNetworkLayer.createDatagramSocket()");
+        return new DatagramSocket();
+    }
+
+    public DatagramSocket createDatagramSocket(int port, InetAddress laddr)
+            throws SocketException {
+        System.out.println("DefaultNetworkLayer.createDatagramSocket(" + port + ", " + laddr + ")");
+        return new DatagramSocket(port, laddr);
+    }
+
+}
