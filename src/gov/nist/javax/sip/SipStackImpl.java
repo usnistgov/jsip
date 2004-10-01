70d53a2ed7ea843b286fe905694753dabfc19cdf
M. Ranganathan <mranga@nist.gov>
2004-10-01 16:05:08 +0000

    Submitted by:  mranga Fixed memory leak

--- src/gov/nist/javax/sip/SipStackImpl.java
+++ src/gov/nist/javax/sip/SipStackImpl.java
@@ -171,7 +171,7 @@ import sim.java.net.*;
  *
  *</ul>
  * 
- * @version JAIN-SIP-1.1 $Revision: 1.36 $ $Date: 2004-09-28 04:07:03 $
+ * @version JAIN-SIP-1.1 $Revision: 1.37 $ $Date: 2004-10-01 16:05:08 $
  * 
  * @author M. Ranganathan <mranga@nist.gov>  <br/>
  *
@@ -764,14 +764,30 @@ public class SipStackImpl
 	}
 
 
-	protected void putPendingTransaction( SIPServerTransaction tr ) {
-		synchronized (pendingTransactions) {
-			this.pendingTransactions.add(tr);
-		}
-	}
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.36  2004/09/28 04:07:03  mranga
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  mranga
+ * Reviewed by:
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  * Revision 1.35  2004/09/27 18:51:18  mranga
  * Reviewed by:   mranga
  *
