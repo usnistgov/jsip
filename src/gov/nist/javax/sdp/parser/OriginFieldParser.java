a53996982e0140ac12fe6cfb656a8c74ab70c464
M. Ranganathan <mranga@nist.gov>
2004-10-21 14:57:17 +0000

    Reviewed by:   mranga
    Fixed origin field parser for sdp.

--- src/gov/nist/javax/sdp/parser/OriginFieldParser.java
+++ src/gov/nist/javax/sdp/parser/OriginFieldParser.java
@@ -6,7 +6,7 @@ import java.text.*;
 
 /**
  * @author  deruelle
- * @version JAIN-SDP-PUBLIC-RELEASE $Revision: 1.2 $ $Date: 2004-01-22 13:26:28 $
+ * @version JAIN-SDP-PUBLIC-RELEASE $Revision: 1.3 $ $Date: 2004-10-21 14:57:17 $
  */
 public class OriginFieldParser extends SDPParser {
 
@@ -24,7 +24,7 @@ public class OriginFieldParser extends SDPParser {
 			lexer.match('=');
 			lexer.SPorHT();
 
-			lexer.match(LexerCore.ID);
+			lexer.match(LexerCore.SAFE);
 			Token userName = lexer.getNextToken();
 			originField.setUsername(userName.getTokenValue());
 			this.lexer.SPorHT();
@@ -69,7 +69,7 @@ public class OriginFieldParser extends SDPParser {
 		String origin[] =
 			{
 				"o=4855 13760799956958020 13760799956958020 IN IP4 166.35.224.216\r\n",
-				"o=mhandley 2890844526 2890842807 IN IP4 126.16.64.4\n",
+				"o=mh/andley 2890844526 2890842807 IN IP4 126.16.64.4\n",
 				"o=UserB 2890844527 2890844527 IN IP4 everywhere.com\n",
 				"o=UserA 2890844526 2890844526 IN IP4 here.com\n",
 				"o=IFAXTERMINAL01 2890844527 2890844527 IN IP4 ift.here.com\n",
@@ -88,4 +88,28 @@ public class OriginFieldParser extends SDPParser {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/01/22 13:26:28  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
