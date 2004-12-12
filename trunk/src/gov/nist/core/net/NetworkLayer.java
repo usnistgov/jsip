01526e51ada144c7e342d93394da1f21cfa7091d
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2004-12-12 23:47:19 +0000

    Issue number:  46 Submitted by:  espen Reviewed by:   M. Ranganathan
    Use the local ip address when creating outbound TCP and TLS sockets.

--- trunk/src/gov/nist/core/net/NetworkLayer.java
+++ trunk/src/gov/nist/core/net/NetworkLayer.java
@@ -53,6 +53,17 @@ public interface NetworkLayer {
      * @return the socket
      */
     public Socket createSocket(InetAddress address, int port) throws IOException;
+
+    /**
+     * Creates a stream socket and connects it to the specified port number at the specified IP address.
+     * comparable to "new java.net.Socket(address, port,localaddress);"
+     * 
+     * @param address
+     * @param port
+     * @param localAddress
+     * @return the socket
+     */
+    public Socket createSocket(InetAddress address, int port, InetAddress localAddress) throws IOException;
     
     /**
      * Creates a stream SSL socket and connects it to the specified port number at the specified IP address.
@@ -63,6 +74,17 @@ public interface NetworkLayer {
      * @return the socket
      */
     public SSLSocket createSSLSocket(InetAddress address, int port) throws IOException;
+
+    /**
+     * Creates a stream SSL socket and connects it to the specified port number at the specified IP address.
+     * Added by Daniel J. Martinez Manzano <dani@dif.um.es>
+     *
+     * @param address
+     * @param port
+     * @param localAddress -- my address.
+     * @return the socket
+     */
+    public SSLSocket createSSLSocket(InetAddress address, int port, InetAddress localAddress) throws IOException;
 
     /**
      * Constructs a datagram socket and binds it to any available port on the local host machine.
