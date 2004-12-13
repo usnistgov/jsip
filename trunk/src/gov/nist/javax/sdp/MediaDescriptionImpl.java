a0e8558ed4993e90ad86c2f06e101c475382fe28
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2004-12-13 02:39:34 +0000

    Reviewed by:   mranga javadoc bug

--- trunk/src/gov/nist/javax/sdp/MediaDescriptionImpl.java
+++ trunk/src/gov/nist/javax/sdp/MediaDescriptionImpl.java
@@ -325,8 +325,8 @@ public class MediaDescriptionImpl implements javax.sdp.MediaDescription {
 	}
 
 	/** Adds the specified Attribute to this Description object.
-	 * @param Attributes  the attribute to add
-	 * @throws SdpException if the attributes is null
+	 * @param attributes  -- the attribute to add
+	 * @throws SdpException -- if the attributes is null
 	 */
 	public void setAttributes(Vector attributes) throws SdpException {
 		this.attributeFields = attributes;
@@ -533,4 +533,28 @@ public class MediaDescriptionImpl implements javax.sdp.MediaDescription {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/01/22 13:26:27  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
