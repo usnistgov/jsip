b1c301db52cb59a101cc9171071b4182063d417c
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2005-03-27 14:00:14 +0000

    Issue number: Obtained from: Submitted by:  mranga Reviewed by:   mranga
    Added example
    CVS: ----------------------------------------------------------------------
    CVS: Issue number:
    CVS:   If this change addresses one or more issues,
    CVS:   then enter the issue number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to the project by someone else; i.e.,
    CVS:   they sent us a patch or a set of diffs, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.

--- trunk/src/gov/nist/javax/sip/parser/ReferToParser.java
+++ trunk/src/gov/nist/javax/sip/parser/ReferToParser.java
@@ -6,7 +6,7 @@ import gov.nist.javax.sip.header.*;
 /**
  * To Header parser.
  *
- * @version JAIN-SIP-1.1 $Revision: 1.3 $ $Date: 2004-01-22 13:26:31 $
+ * @version JAIN-SIP-1.1 $Revision: 1.4 $ $Date: 2005-03-27 14:00:14 $
  *
  * @author Olivier Deruelle <deruelle@nist.gov>  <br/>
  *
@@ -37,7 +37,8 @@ public class ReferToParser extends AddressParametersParser {
 
 	public static void main(String args[]) throws ParseException {
 		String to[] =
-			{
+			{   "Refer-To: <sip:dave@denver.example.org?" +
+					"Replaces=12345%40192.168.118.3%3Bto-tag%3D12345%3Bfrom-tag%3D5FFE-3994>\n",
 				"Refer-To: <sip:+1-650-555-2222@ss1.wcom.com;user=phone>;tag=5617\n",
 				"Refer-To: T. A. Watson <sip:watson@bell-telephone.com>\n",
 				"Refer-To: LittleGuy <sip:UserB@there.com>\n",
@@ -53,4 +54,28 @@ public class ReferToParser extends AddressParametersParser {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2004/01/22 13:26:31  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
