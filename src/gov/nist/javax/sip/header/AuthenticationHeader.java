adff43f6e55a5181793a46965adef92ceb704006
dmuresan <dmuresan@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2005-04-04 09:54:57 +0000

    Some constants declared final.

--- src/gov/nist/javax/sip/header/AuthenticationHeader.java
+++ src/gov/nist/javax/sip/header/AuthenticationHeader.java
@@ -11,26 +11,26 @@ import java.text.ParseException;
  *
  * @author Olivier Deruelle <deruelle@nist.gov>
  * @author M. Ranganathan <mranga@nist.gov><br/>
- * @version JAIN-SIP-1.1 $Revision: 1.2 $ $Date: 2004-01-22 13:26:29 $
+ * @version JAIN-SIP-1.1 $Revision: 1.3 $ $Date: 2005-04-04 09:54:57 $
  *
  * <a href="{@docRoot}/uncopyright.html">This code is in the public domain.</a>
  */
 public abstract class AuthenticationHeader extends ParametersHeader {
 
-	public static String DOMAIN = ParameterNames.DOMAIN;
-	public static String REALM = ParameterNames.REALM;
-	public static String OPAQUE = ParameterNames.OPAQUE;
-	public static String ALGORITHM = ParameterNames.ALGORITHM;
-	public static String QOP = ParameterNames.QOP;
-	public static String STALE = ParameterNames.STALE;
-	public static String SIGNATURE = ParameterNames.SIGNATURE;
-	public static String RESPONSE = ParameterNames.RESPONSE;
-	public static String SIGNED_BY = ParameterNames.SIGNED_BY;
-	public static String NC = ParameterNames.NC;
-	public static String URI = ParameterNames.URI;
-	public static String USERNAME = ParameterNames.USERNAME;
-	public static String CNONCE = ParameterNames.CNONCE;
-	public static String NONCE = ParameterNames.NONCE;
+	public static final String DOMAIN = ParameterNames.DOMAIN;
+	public static final String REALM = ParameterNames.REALM;
+	public static final String OPAQUE = ParameterNames.OPAQUE;
+	public static final String ALGORITHM = ParameterNames.ALGORITHM;
+	public static final String QOP = ParameterNames.QOP;
+	public static final String STALE = ParameterNames.STALE;
+	public static final String SIGNATURE = ParameterNames.SIGNATURE;
+	public static final String RESPONSE = ParameterNames.RESPONSE;
+	public static final String SIGNED_BY = ParameterNames.SIGNED_BY;
+	public static final String NC = ParameterNames.NC;
+	public static final String URI = ParameterNames.URI;
+	public static final String USERNAME = ParameterNames.USERNAME;
+	public static final String CNONCE = ParameterNames.CNONCE;
+	public static final String NONCE = ParameterNames.NONCE;
 
 	protected String scheme;
 
@@ -425,4 +425,28 @@ public abstract class AuthenticationHeader extends ParametersHeader {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/01/22 13:26:29  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
