55364eff49ebb1ed9fffded8c2f48e244388e76f
Muresan Daniel <danymuresan91@gmail.com>
2005-04-04 10:01:28 +0000

    Used StringBuffer instead of String += for concatenation in various encode() methods in javax.sdp.

--- src/gov/nist/javax/sdp/fields/SDPObjectList.java
+++ src/gov/nist/javax/sdp/fields/SDPObjectList.java
@@ -151,14 +151,14 @@ public class SDPObjectList extends GenericObjectList {
 	}
 
 	public String encode() {
-		String retval = "";
+		StringBuffer retval = new StringBuffer();
 		SDPObject sdpObject;
 		for (sdpObject = (SDPObject) this.first();
 			sdpObject != null;
 			sdpObject = (SDPObject) this.next()) {
-			retval += sdpObject.encode();
+			retval.append (sdpObject.encode());
 		}
-		return retval;
+		return retval.toString();
 	}
 
 	public String toString() {
@@ -307,4 +307,28 @@ public class SDPObjectList extends GenericObjectList {
 
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2004/01/22 13:26:27  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
