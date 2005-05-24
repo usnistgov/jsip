5a0ff3a0c28eb7bcf8bb4d39751167c1516ad123
djmartinez <djmartinez@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2005-05-24 08:50:49 +0000

    Added code for supporting correctly IPv6 addresses in Via headers.

--- src/gov/nist/javax/sip/header/HeaderFactoryImpl.java
+++ src/gov/nist/javax/sip/header/HeaderFactoryImpl.java
@@ -13,7 +13,7 @@ import gov.nist.javax.sip.address.*;
 
 /** Implementation of the JAIN SIP  HeaderFactory
 * 
-* @version JAIN-SIP-1.1 $Revision: 1.3 $ $Date: 2004-01-22 13:26:29 $
+* @version JAIN-SIP-1.1 $Revision: 1.4 $ $Date: 2005-05-24 08:50:49 $
 *
 *@author M. Ranganathan <mranga@nist.gov>  <br/>
 *@author Olivier Deruelle <deruelle@nist.gov><br/>
@@ -971,6 +971,13 @@ public class HeaderFactoryImpl implements HeaderFactory {
 		Via via = new Via();
 		if (branch != null)
 			via.setBranch(branch);
+
+		// Added by Daniel Martinez <dani@dif.um.es>
+		// for supporting IPv6 addresses
+		if(host.indexOf(':') >= 0 &&
+		   host.indexOf('[') < 0)
+			host = '[' + host + ']';
+
 		via.setHost(host);
 		via.setPort(port);
 		via.setTransport(transport);
@@ -1125,4 +1132,28 @@ public class HeaderFactoryImpl implements HeaderFactory {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2004/01/22 13:26:29  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
