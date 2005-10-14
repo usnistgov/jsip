65951f7056628f0ada5f26762f4f42dbffef61e6
Jeroen van Bemmel <jbemmel@users.noreply.github.com>
2005-10-14 18:24:42 +0000

    return "" (not null) for flag attributes such as 'recvonly', such that applications can easily distinguish between this parameter being present or not

--- src/gov/nist/javax/sdp/parser/AttributeFieldParser.java
+++ src/gov/nist/javax/sdp/parser/AttributeFieldParser.java
@@ -11,7 +11,7 @@ import java.text.*;
 
 /**
  * @author  Olivier Deruelle
- * @version JAIN-SDP-PUBLIC-RELEASE $Revision: 1.2 $ $Date: 2004-01-22 13:26:28 $
+ * @version JAIN-SDP-PUBLIC-RELEASE $Revision: 1.3 $ $Date: 2005-10-14 18:24:42 $
  */
 public class AttributeFieldParser extends SDPParser {
 
@@ -47,7 +47,7 @@ public class AttributeFieldParser extends SDPParser {
 					throw new ParseException(
 						this.lexer.getBuffer(),
 						this.lexer.getPtr());
-				nameValue = new NameValue(rest.trim(), null);
+				nameValue = new NameValue(rest.trim(), "" );	// jvB: use empty string for flag values
 			}
 			attributeField.setAttribute(nameValue);
 
@@ -98,4 +98,28 @@ public class AttributeFieldParser extends SDPParser {
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/01/22 13:26:28  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
