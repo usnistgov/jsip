e2ae9f57229cc3e28a887d59572bbf0717e1a54c
Jeroen van Bemmel <jbemmel@users.noreply.github.com>
2005-10-14 22:41:57 +0000

    fixed wrong parameter name

--- src/gov/nist/javax/sip/header/AuthenticationInfo.java
+++ src/gov/nist/javax/sip/header/AuthenticationInfo.java
@@ -7,7 +7,7 @@ import java.text.ParseException;
  * Authentication info SIP Header.
  *
  * @author M. Ranganathan <mranga@nist.gov>  NIST/ITL/ANTD
- * @version JAIN-SIP-1.1 $Revision: 1.2 $ $Date: 2004-01-22 13:26:29 $
+ * @version JAIN-SIP-1.1 $Revision: 1.3 $ $Date: 2005-10-14 22:41:57 $
  *
  * <a href="{@docRoot}/uncopyright.html">This code is in the public domain.</a>
  */
@@ -172,7 +172,7 @@ public class AuthenticationInfo
 	 * @since v1.1
 	 */
 	public void setResponse(String response) throws ParseException {
-		this.setParameter(ParameterNames.RESPONSE, response);
+		this.setParameter(ParameterNames.RESPONSE_AUTH, response);
 	}
 
 	public void setParameter(String name, String value) throws ParseException {
@@ -206,4 +206,28 @@ public class AuthenticationInfo
 }
 /*
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2004/01/22 13:26:29  sverker
+ * Issue number:
+ * Obtained from:
+ * Submitted by:  sverker
+ * Reviewed by:   mranga
+ *
+ * Major reformat of code to conform with style guide. Resolved compiler and javadoc warnings. Added CVS tags.
+ *
+ * CVS: ----------------------------------------------------------------------
+ * CVS: Issue number:
+ * CVS:   If this change addresses one or more issues,
+ * CVS:   then enter the issue number(s) here.
+ * CVS: Obtained from:
+ * CVS:   If this change has been taken from another system,
+ * CVS:   then name the system in this line, otherwise delete it.
+ * CVS: Submitted by:
+ * CVS:   If this code has been contributed to the project by someone else; i.e.,
+ * CVS:   they sent us a patch or a set of diffs, then include their name/email
+ * CVS:   address here. If this is your work then delete this line.
+ * CVS: Reviewed by:
+ * CVS:   If we are doing pre-commit code reviews and someone else has
+ * CVS:   reviewed your changes, include their name(s) here.
+ * CVS:   If you have not had it reviewed then delete this line.
+ *
  */
