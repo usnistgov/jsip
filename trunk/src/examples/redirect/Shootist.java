ebebb017759ace2ddafdc8b2580ae1637ef52593
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2006-08-16 00:02:14 +0000

    Issue number: Obtained from: Submitted by:  mranga Reviewed by:   mranga Minor fixes CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

--- trunk/src/examples/redirect/Shootist.java
+++ trunk/src/examples/redirect/Shootist.java
@@ -56,15 +56,7 @@ public class Shootist extends TestCase implements SipListener {
 	
 	private static Logger logger = Logger.getLogger(Shootist.class);
 
-	protected static final String usageString = "java "
-			+ "examples.shootist.Shootist \n"
-			+ ">>>> is your class path set to the root?";
-
-	private static void usage() {
-		logger.info(usageString);
-		System.exit(0);
-
-	}
+	
 
 	public void processRequest(RequestEvent requestReceivedEvent) {
 		Request request = requestReceivedEvent.getRequest();
--- trunk/src/examples/reinvite/Shootme.java
+++ trunk/src/examples/reinvite/Shootme.java
@@ -59,11 +59,7 @@ public class Shootme  implements SipListener {
 			+ "examples.shootist.Shootist \n"
 			+ ">>>> is your class path set to the root?";
 
-	private static void usage() {
-		logger.info(usageString);
-		System.exit(0);
-
-	}
+	
 
 	public void processRequest(RequestEvent requestEvent) {
 		Request request = requestEvent.getRequest();
