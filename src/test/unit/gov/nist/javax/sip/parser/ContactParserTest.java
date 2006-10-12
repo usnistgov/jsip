3338fd296b7350a5ba6749efc908f0e0e42cc7a7
pmusgrave <pmusgrave@users.noreply.github.com>
2006-10-12 12:01:33 +0000

    Issue number:  79,80 (Extension headers) Submitted by:  pmusgrave@newheights.com Added Junit tests for extension headers and corrected the Route JUnit test. Extensed the Contact test to check it does sip-instance and gruu params.

--- src/test/unit/gov/nist/javax/sip/parser/ContactParserTest.java
+++ src/test/unit/gov/nist/javax/sip/parser/ContactParserTest.java
@@ -51,7 +51,9 @@ public class ContactParserTest extends ParserTestCase {
 						+ "\n",
 				"Contact:*\n",
 				"Contact:BigGuy<sip:utente@127.0.0.1;5000>;Expires=3600\n" ,
-				"Contact: sip:nobody@192.168.0.241;expires=600;q=0.5\n"};
+				"Contact: sip:nobody@192.168.0.241;expires=600;q=0.5\n",
+				// pmusgrave - add +sip-instance tests (outbound & gruu drafts)
+				"Contact: <sip:callee@192.0.2.1>;+sip-instance=\"<urn:uid:f81d-5463>\"\n" };
 		super.testParser(ContactParser.class, contact);
 
 	}
