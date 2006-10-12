a2204e7f9fd66823822d788beb7c9f8b206bbd35
pmusgrave <pmusgrave@users.noreply.github.com>
2006-10-12 12:01:33 +0000

    Issue number:  79,80 (Extension headers) Submitted by:  pmusgrave@newheights.com Added Junit tests for extension headers and corrected the Route JUnit test. Extensed the Contact test to check it does sip-instance and gruu params.

--- src/test/unit/gov/nist/javax/sip/parser/RouteParserTest.java
+++ src/test/unit/gov/nist/javax/sip/parser/RouteParserTest.java
@@ -19,7 +19,7 @@
 */
 package test.unit.gov.nist.javax.sip.parser;
 
-import gov.nist.javax.sip.parser.AcceptLanguageParser;
+
 import gov.nist.javax.sip.parser.RouteParser;
 
 public class RouteParserTest extends ParserTestCase {
@@ -28,10 +28,13 @@ public class RouteParserTest extends ParserTestCase {
 		// TODO Auto-generated method stub
 		String[] routes =  {
 		 "Route: <sip:alice@atlanta.com>\n",
+		 "Route: \"AliceA\" <sip:alice@atlanta.com>\n",
+		 // technically illegal >
 	     "Route: sip:bob@biloxi.com \n",
 	     "Route: <sip:TIA-P25-U2Uorig@AF.003.1300.p25dr;lr>\n",
-	     "Route: sip:alice@atlanta.com, sip:bob@biloxi.com, sip:carol@chicago.com\n"};
-		super.testParser(RouteParser.class,routes);
+	     // pmusgrave - illegal (must have <>'s )
+	     // "Route: sip:alice@atlanta.com, sip:bob@biloxi.com, sip:carol@chicago.com\n",
+		"Route: <sip:bigbox3.site3.atlanta.com;lr>,<sip:server10.biloxi.com;lr>\n"};		super.testParser(RouteParser.class,routes);
 	}
 
 }
