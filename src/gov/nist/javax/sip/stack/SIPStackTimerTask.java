a94c82da58417f3432f7e909e09a2160d24c80e2
M. Ranganathan <mranga@nist.gov>
2007-01-08 19:41:48 +0000

    Issue number: 95 Obtained from: Submitted by:  hagai_sela Reviewed by:  mranga
    SIPStackTimerTask now catches Throwable instead of Exception
    
    
    CVS: ----------------------------------------------------------------------
    CVS: Issue number:
    CVS:   If this change addresses one or more issues,
    CVS:   then enter the issue number(s) here.
    CVS: Obtained from:
    CVS:   If this change has been taken from another system,
    CVS:   then name the system in this line, otherwise delete it.
    CVS: Submitted by:
    CVS:   If this code has been contributed to the project by someone else; i.e.,
    CVS:   they sent us a patch or a set of diffs, then include their name/email
    CVS:   address here. If this is your work then delete this line.
    CVS: Reviewed by:
    CVS:   If we are doing pre-commit code reviews and someone else has
    CVS:   reviewed your changes, include their name(s) here.
    CVS:   If you have not had it reviewed then delete this line.

--- src/gov/nist/javax/sip/stack/SIPStackTimerTask.java
+++ src/gov/nist/javax/sip/stack/SIPStackTimerTask.java
@@ -1,7 +1,7 @@
 /*
  * @author:     Brett Buckingham
  * @author:     Last modified by: $Author: mranga $
- * @version:    $Date: 2006-10-05 16:53:32 $ $Revision: 1.1 $
+ * @version:    $Date: 2007-01-08 19:41:48 $ $Revision: 1.2 $
  *
  * This source code has been contributed to the public domain.
  */
@@ -11,27 +11,25 @@ package gov.nist.javax.sip.stack;
 import java.util.TimerTask;
 
 /**
- * A subclass of TimerTask which runs TimerTask code within a try/catch block to avoid killing the SIPTransactionStack
- * timer thread.  Note: subclasses MUST not override run(); instead they should override runTask().
+ * A subclass of TimerTask which runs TimerTask code within a try/catch block to
+ * avoid killing the SIPTransactionStack timer thread. Note: subclasses MUST not
+ * override run(); instead they should override runTask().
  * 
  * @author Brett Buckingham
- *
+ * 
  */
-public abstract class SIPStackTimerTask extends TimerTask
-{
-  /// Implements code to be run when the SIPStackTimerTask is executed.
-  protected abstract void runTask();
+public abstract class SIPStackTimerTask extends TimerTask {
+	// / Implements code to be run when the SIPStackTimerTask is executed.
+	protected abstract void runTask();
 
-  /// The run() method is final to ensure that all subclasses inherit the exception handling.
-  public final void run()
-  {
-    try
-    {
-      runTask();
-    } catch (Exception e)
-    {
-      System.out.println("SIP stack timer task failed due to exception:");
-      e.printStackTrace();
-    }
-  }
+	// / The run() method is final to ensure that all subclasses inherit the
+	// exception handling.
+	public final void run() {
+		try {
+			runTask();
+		} catch (Throwable e) {
+			System.out.println("SIP stack timer task failed due to exception:");
+			e.printStackTrace();
+		}
+	}
 }
