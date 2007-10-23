a54bc6207265f3da1b93f148b4d8675038b1be32
M. Ranganathan <mranga@nist.gov>
2007-10-23 18:07:50 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:   mranga
    fixed compile error.

--- src/gov/nist/javax/sip/header/ExtensionHeaderList.java
+++ src/gov/nist/javax/sip/header/ExtensionHeaderList.java
@@ -31,10 +31,11 @@ package gov.nist.javax.sip.header;
 import java.util.ListIterator;
 
 import javax.sip.header.ExtensionHeader;
+import javax.sip.header.Header;
 
 /**
  * A generic extension header list.
- * @version 1.2 $Revision: 1.5 $ $Date: 2007-10-23 17:34:51 $
+ * @version 1.2 $Revision: 1.6 $ $Date: 2007-10-23 18:07:50 $
  * @since 1.1
  */
 public class ExtensionHeaderList extends SIPHeaderList<ExtensionHeader> { 
