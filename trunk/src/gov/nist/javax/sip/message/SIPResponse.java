4c226568a0dd1714de64a7e9c1a51afddf5e2bdb
jbemmel <jbemmel@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2008-01-24 19:43:40 +0000

    Removed createRequest() that was only used for createAck

--- trunk/src/gov/nist/javax/sip/message/SIPResponse.java
+++ trunk/src/gov/nist/javax/sip/message/SIPResponse.java
@@ -49,7 +49,7 @@ import javax.sip.message.Request;
 /**
  * SIP Response structure.
  *
- * @version 1.2 $Revision: 1.16 $ $Date: 2007-10-29 02:24:37 $
+ * @version 1.2 $Revision: 1.17 $ $Date: 2008-01-24 19:43:40 $
  * @since 1.1
  *
  * @author M. Ranganathan   <br/>
@@ -650,8 +650,7 @@ public final class SIPResponse
 	 *@param via is the via header to use.
 	 *@param cseq is the cseq header to use in the generated
 	 * request.
-	 */
-
+	 *
 	public SIPRequest createRequest(SipUri requestURI, Via via, CSeq cseq) {
 		SIPRequest newRequest = new SIPRequest();
 		String method = cseq.getMethod();
@@ -695,7 +694,7 @@ public final class SIPResponse
 		  
 		}
 		return newRequest;
-	}
+	}*/
 
 	/**
 	 * Get the encoded first line.
