31fa16e219417e9e3b61bcd92e6fa7c51ecc4adb
M. Ranganathan <mranga@nist.gov>
2008-03-17 16:19:27 +0000

    Issue number: Obtained from: Submitted by:  mranga Reviewed by:   mranga Additional support in Dialog to be included in next release.

---
+++ src/gov/nist/javax/sip/stack/DialogExt.java
@@ -0,0 +1,17 @@
+package gov.nist.javax.sip.stack;
+
+import javax.sip.SipProvider;
+
+/**
+ * Extensions for Next specification revision. These interfaces will remain unchanged
+ * and be merged with the next revision of the spec.
+ * 
+ * 
+ * @author mranga
+ *
+ */
+public interface DialogExt {
+	
+	public SipProvider getSipProvider() ;
+
+}
