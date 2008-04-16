41ba73c63ffd762f15838aa1ec0ca9ddb7f5b3fd
M. Ranganathan <mranga@nist.gov>
2008-04-16 03:12:23 +0000

    Issue number: Obtained from: Submitted by:  mranga Reviewed by:   mranga Add a listening point method to send keepalives ( for NAT travesal ).

--- src/gov/nist/javax/sip/ListeningPointExt.java
+++ src/gov/nist/javax/sip/ListeningPointExt.java
@@ -1,5 +1,7 @@
 package gov.nist.javax.sip;
 
+import java.io.IOException;
+
 import javax.sip.header.ContactHeader;
 
 public interface ListeningPointExt {
@@ -14,6 +16,15 @@ public interface ListeningPointExt {
 	 */
 	
 	ContactHeader createContactHeader() ;
+	
+	/**
+	 * Send a heartbeat to the specified Ip address and port
+	 * via this listening point. This method can be used to send out a period
+	 * CR-LF for NAT keepalive.
+	 * 
+	 * @since 2.0
+	 */
+	public void sendHeartbeat(String ipAddress, int port) throws IOException ;
 	
 
 }
