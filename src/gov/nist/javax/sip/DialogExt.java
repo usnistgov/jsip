51a7f0d05e35431b65a737dbb642750af9e67d82
M. Ranganathan <mranga@nist.gov>
2009-08-04 20:59:21 +0000

    Issue number:  207 Adds a new flag to enforce strict sequencing of re-INVITEs sent to a target. This avoids the possiblity of getting 491 Request Pending from the target.

--- src/gov/nist/javax/sip/DialogExt.java
+++ src/gov/nist/javax/sip/DialogExt.java
@@ -19,5 +19,20 @@ public interface DialogExt {
      * @since 2.0
      */
     public SipProvider getSipProvider() ;
+    
+    /**
+     * Set a flag that instructs the implementation to take care of sequencing of re-INVITES.
+     * If this flag is set then all the re-INVITEs that use the dialog will 
+     * wait till an ACK has been sent on the previous re-INVITE before sending the
+     * new re-INVITE. This prevents the UA recieving the re-INVITE from receiving an
+     * out of order INVITE - which would result in it sending a 493 with a timeout
+     * to indicate that the request should be re-transmitted. If this flag is set
+     * the dialog.sendRequest works asynchronously. An error is reported on timeout
+     * if the ACK is not sent within 8 seconds.
+     * 
+     * @since 2.0
+     */
+    public void setAllowReInviteInterleaving(boolean flag);
+    
 
 }
