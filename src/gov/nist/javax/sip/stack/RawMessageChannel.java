65f6114858f49de5357a1d1a428d898c27430fc6
nobody <nobody@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2009-09-21 06:57:03 +0000

    This commit was manufactured by cvs2svn to create branch 'SCTP'.

--- src/gov/nist/javax/sip/stack/RawMessageChannel.java
+++
@@ -1,9 +0,0 @@
-package gov.nist.javax.sip.stack;
-
-import gov.nist.javax.sip.message.SIPMessage;
-
-public interface RawMessageChannel {
-
-    public abstract void processMessage(SIPMessage sipMessage) throws Exception ;
-
-}
