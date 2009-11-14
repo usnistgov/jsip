5fa96be5df1ba88c428dbcfab8cdf0d3fde625b4
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2009-11-14 18:54:20 +0000

    Code cleanup : Fix a findbugs warning.

--- trunk/src/gov/nist/core/DuplicateNameValueList.java
+++ trunk/src/gov/nist/core/DuplicateNameValueList.java
@@ -1,3 +1,26 @@
+/*
+ * Conditions Of Use
+ *
+ * This software was developed by employees of the National Institute of
+ * Standards and Technology (NIST), an agency of the Federal Government.
+ * Pursuant to title 15 Untied States Code Section 105, works of NIST
+ * employees are not subject to copyright protection in the United States
+ * and are considered to be in the public domain.  As a result, a formal
+ * license is not needed to use the software.
+ *
+ * This software is provided by NIST as a service and is expressly
+ * provided "AS IS."  NIST MAKES NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED
+ * OR STATUTORY, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT
+ * AND DATA ACCURACY.  NIST does not warrant or make any representations
+ * regarding the use of the software or the results thereof, including but
+ * not limited to the correctness, accuracy, reliability or usefulness of
+ * the software.
+ *
+ * Permission to use this software is contingent upon your acceptance
+ * of the terms of this agreement.
+ *
+ */
 package gov.nist.core;
 
 import java.io.Serializable;
@@ -6,250 +29,252 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-
 /**
  * 
+ * 
+ * This is a Duplicate Name Value List that will allow multiple values map to the same key.
+ * 
+ * The parsing and encoding logic for it is the same as that of NameValueList. Only the HashMap
+ * container is different.
+ * 
  * @author aayush.bhatnagar
+ * @since 2.0
  * 
- * This is a Duplicate Name Value List that will allow multiple values map
- * to the same key. 
- * 
- * The parsing and encoding logic for it is the 
- * same as that of NameValueList. Only the HashMap container is different.
- *
  */
-public class DuplicateNameValueList implements Serializable{
+public class DuplicateNameValueList implements Serializable, Cloneable {
 
-	MultiHashMap m = new MultiHashMap();
-	 private String separator;
-	
-	private static final long serialVersionUID = -5611332957903796952L;
+    private MultiHashMap nameValueMap = new MultiHashMap();
+    private String separator;
 
-	public DuplicateNameValueList()
-	
-	{
-		this.separator = ";";
-	}
-	
-	//------------------
-	
-	   public void setSeparator(String separator) {
-	        this.separator = separator;
-	    }
+    private static final long serialVersionUID = -5611332957903796952L;
 
-	    /**
-	     * Encode the list in semicolon separated form.
-	     *
-	     * @return an encoded string containing the objects in this list.
-	     * @since v1.0
-	     */
-	    public String encode() {
-	        return encode(new StringBuffer()).toString();
-	    }
+    public DuplicateNameValueList()
 
-	    public StringBuffer encode(StringBuffer buffer) {
-	        if (!m.isEmpty()) {
-	            Iterator<NameValue> iterator = m.values().iterator();
-	            if (iterator.hasNext()) {
-	                while (true) {
-	                    Object obj = iterator.next();
-	                    if (obj instanceof GenericObject) {
-	                        GenericObject gobj = (GenericObject) obj;
-	                        gobj.encode(buffer);
-	                    } else {
-	                        buffer.append(obj.toString());
-	                    }
-	                    if (iterator.hasNext())
-	                        buffer.append(separator);
-	                    else
-	                        break;
-	                }
-	            }
-	        }
-	        return buffer;
-	    }
+    {
+        this.separator = ";";
+    }
 
-	    public String toString() {
-	        return this.encode();
-	    }
+    // ------------------
 
-	    /**
-	     * Set a namevalue object in this list.
-	     */
+    public void setSeparator(String separator) {
+        this.separator = separator;
+    }
 
-	    public void set(NameValue nv) {
-	        this.m.put(nv.getName().toLowerCase(), nv);
-	    }
+    /**
+     * Encode the list in semicolon separated form.
+     * 
+     * @return an encoded string containing the objects in this list.
+     * @since v1.0
+     */
+    public String encode() {
+        return encode(new StringBuffer()).toString();
+    }
 
-	    /**
-	     * Set a namevalue object in this list.
-	     */
-	    public void set(String name, Object value) {
-	        NameValue nameValue = new NameValue(name, value);
-	        m.put(name.toLowerCase(), nameValue);
+    public StringBuffer encode(StringBuffer buffer) {
+        if (!nameValueMap.isEmpty()) {
+            Iterator<NameValue> iterator = nameValueMap.values().iterator();
+            if (iterator.hasNext()) {
+                while (true) {
+                    Object obj = iterator.next();
+                    if (obj instanceof GenericObject) {
+                        GenericObject gobj = (GenericObject) obj;
+                        gobj.encode(buffer);
+                    } else {
+                        buffer.append(obj.toString());
+                    }
+                    if (iterator.hasNext())
+                        buffer.append(separator);
+                    else
+                        break;
+                }
+            }
+        }
+        return buffer;
+    }
 
-	    }
+    public String toString() {
+        return this.encode();
+    }
 
-	    /**
-	     * Compare if two NameValue lists are equal.
-	     *
-	     * @param otherObject
-	     *            is the object to compare to.
-	     * @return true if the two objects compare for equality.
-	     */
-	    public boolean equals(Object otherObject) {
-	        if (!otherObject.getClass().equals(this.getClass())) {
-	            return false;
-	        }
-	        DuplicateNameValueList other = (DuplicateNameValueList) otherObject;
+    /**
+     * Set a namevalue object in this list.
+     */
 
-	        if (m.size() != other.m.size()) {
-	            return false;
-	        }
-	        Iterator<String> li = this.m.keySet().iterator();
+    public void set(NameValue nv) {
+        this.nameValueMap.put(nv.getName().toLowerCase(), nv);
+    }
 
-	        while (li.hasNext()) {
-	            String key = (String) li.next();
-	            Collection nv1 = this.getNameValue(key);
-	            Collection nv2 = (Collection) other.m.get(key);
-	            if (nv2 == null)
-	                return false;
-	            else if (!nv2.equals(nv1))
-	                return false;
-	        }
-	        return true;
-	    }
+    /**
+     * Set a namevalue object in this list.
+     */
+    public void set(String name, Object value) {
+        NameValue nameValue = new NameValue(name, value);
+        nameValueMap.put(name.toLowerCase(), nameValue);
 
-	    /**
-	     * Do a lookup on a given name and return value associated with it.
-	     */
-	    public Object getValue(String name) {
-	    	Collection nv = this.getNameValue(name.toLowerCase());
-	        if (nv != null)
-	            return nv;
-	        else
-	            return null;
-	    }
+    }
 
-	    /**
-	     * Get the NameValue record given a name.
-	     *
-	     * @since 1.0
-	     */
-	    public Collection getNameValue(String name) {
-	        return (Collection) this.m.get(name.toLowerCase());
-	    }
+    /**
+     * Compare if two NameValue lists are equal.
+     * 
+     * @param otherObject is the object to compare to.
+     * @return true if the two objects compare for equality.
+     */
+    public boolean equals(Object otherObject) {
+        if (!otherObject.getClass().equals(this.getClass())) {
+            return false;
+        }
+        DuplicateNameValueList other = (DuplicateNameValueList) otherObject;
 
-	    /**
-	     * Returns a boolean telling if this NameValueList has a record with this
-	     * name
-	     */
-	    public boolean hasNameValue(String name) {
-	        return m.containsKey(name.toLowerCase());
-	    }
+        if (nameValueMap.size() != other.nameValueMap.size()) {
+            return false;
+        }
+        Iterator<String> li = this.nameValueMap.keySet().iterator();
 
-	    /**
-	     * Remove the element corresponding to this name.
-	     */
-	    public boolean delete(String name) {
-	        String lcName = name.toLowerCase();
-	        if (this.m.containsKey(lcName)) {
-	            this.m.remove(lcName);
-	            return true;
-	        } else {
-	            return false;
-	        }
+        while (li.hasNext()) {
+            String key = (String) li.next();
+            Collection nv1 = this.getNameValue(key);
+            Collection nv2 = (Collection) other.nameValueMap.get(key);
+            if (nv2 == null)
+                return false;
+            else if (!nv2.equals(nv1))
+                return false;
+        }
+        return true;
+    }
 
-	    }
+    /**
+     * Do a lookup on a given name and return value associated with it.
+     */
+    public Object getValue(String name) {
+        Collection nv = this.getNameValue(name.toLowerCase());
+        if (nv != null)
+            return nv;
+        else
+            return null;
+    }
 
-	    public Object clone() {
-	        DuplicateNameValueList retval = new DuplicateNameValueList();
-	        retval.setSeparator(this.separator);
-	        Iterator<NameValue> it = this.m.values().iterator();
-	        while (it.hasNext()) {
-	            retval.set((NameValue) ((NameValue) it.next()).clone());
-	        }
-	        return retval;
-	    }
-	    
-	    /**
-	     * Return an iterator for the name-value pairs of this list.
-	     *
-	     * @return the iterator.
-	     */
-	    public Iterator<NameValue> iterator() {
-	        return this.m.values().iterator();
-	    }
+    /**
+     * Get the NameValue record given a name.
+     * 
+     */
+    public Collection getNameValue(String name) {
+        return (Collection) this.nameValueMap.get(name.toLowerCase());
+    }
 
-	    /**
-	     * Get a list of parameter names.
-	     *
-	     * @return a list iterator that has the names of the parameters.
-	     */
-	    public Iterator<String> getNames() {
-	        return this.m.keySet().iterator();
+    /**
+     * Returns a boolean telling if this NameValueList has a record with this name
+     */
+    public boolean hasNameValue(String name) {
+        return nameValueMap.containsKey(name.toLowerCase());
+    }
 
-	    }
+    /**
+     * Remove the element corresponding to this name.
+     */
+    public boolean delete(String name) {
+        String lcName = name.toLowerCase();
+        if (this.nameValueMap.containsKey(lcName)) {
+            this.nameValueMap.remove(lcName);
+            return true;
+        } else {
+            return false;
+        }
 
-	    /**
-	     * Get the parameter as a String.
-	     *
-	     * @return the parameter as a string.
-	     */
-	    public String getParameter(String name) {
-	        Object val = this.getValue(name);
-	        if (val == null)
-	            return null;
-	        if (val instanceof GenericObject)
-	            return ((GenericObject) val).encode();
-	        else
-	            return val.toString();
-	    }
+    }
 
-	public void clear() {
-		m.clear();
-		
-	}
+    public Object clone() {
+        DuplicateNameValueList retval = new DuplicateNameValueList();
+        retval.setSeparator(this.separator);
+        Iterator<NameValue> it = this.nameValueMap.values().iterator();
+        while (it.hasNext()) {
+            retval.set((NameValue) ((NameValue) it.next()).clone());
+        }
+        return retval;
+    }
 
-	public boolean containsKey(Object key) {
-		return m.containsKey(key.toString().toLowerCase());
-	}
+    /**
+     * Return an iterator for the name-value pairs of this list.
+     * 
+     * @return the iterator.
+     */
+    public Iterator<NameValue> iterator() {
+        return this.nameValueMap.values().iterator();
+    }
 
-	public boolean containsValue(Object value) {
-		return m.containsValue(value);
-	}
+    /**
+     * Get a list of parameter names.
+     * 
+     * @return a list iterator that has the names of the parameters.
+     */
+    public Iterator<String> getNames() {
+        return this.nameValueMap.keySet().iterator();
 
-	public NameValue get(Object key) {
-		return (NameValue) this.m.get(key);
-	}
+    }
 
-	public boolean isEmpty() {
-		return this.m.isEmpty();
-	}
+    /**
+     * Get the parameter as a String.
+     * 
+     * @return the parameter as a string.
+     */
+    public String getParameter(String name) {
+        Object val = this.getValue(name);
+        if (val == null)
+            return null;
+        if (val instanceof GenericObject)
+            return ((GenericObject) val).encode();
+        else
+            return val.toString();
+    }
 
-	public Set<String> keySet() {
-		return this.m.keySet();
-	}
+    public void clear() {
+        nameValueMap.clear();
 
-	public NameValue put(String key, NameValue value) {
-		return (NameValue) this.m.put(key, value);
-	}
+    }
 
-	public void putAll(Map<? extends String, ? extends NameValue> t) {
-		this.m.putAll(t);
-		
-	}
+    public boolean containsKey(Object key) {
+        return nameValueMap.containsKey(key.toString().toLowerCase());
+    }
 
-	public NameValue remove(Object key) {
-		return (NameValue) this.m.remove(key);
-	}
+    public boolean containsValue(Object value) {
+        return nameValueMap.containsValue(value);
+    }
 
-	public int size() {
-		return this.m.size();
-	}
+    public NameValue get(Object key) {
+        return (NameValue) this.nameValueMap.get(key);
+    }
 
-	public Collection<NameValue> values() {
-		return this.m.values();
-	}
+    public boolean isEmpty() {
+        return this.nameValueMap.isEmpty();
+    }
+
+    public Set<String> keySet() {
+        return this.nameValueMap.keySet();
+    }
+
+    public NameValue put(String key, NameValue value) {
+        return (NameValue) this.nameValueMap.put(key, value);
+    }
+
+    public void putAll(Map< ? extends String, ? extends NameValue> t) {
+        this.nameValueMap.putAll(t);
+
+    }
+
+    public NameValue remove(Object key) {
+        return (NameValue) this.nameValueMap.remove(key);
+    }
+
+    public int size() {
+        return this.nameValueMap.size();
+    }
+
+    public Collection<NameValue> values() {
+        return this.nameValueMap.values();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.nameValueMap.keySet().hashCode();
+    }
 
 }
