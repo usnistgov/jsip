447eca2567fe8a53b82a4d6ed051703af7ccc131
M. Ranganathan <mranga@nist.gov>
2010-01-12 04:23:35 +0000

    Fix  compile error

--- src/gov/nist/javax/sip/header/extensions/References.java
+++ src/gov/nist/javax/sip/header/extensions/References.java
@@ -18,72 +18,76 @@ public class References extends ParametersHeader  implements ReferencesHeader,Ex
         super(ReferencesHeader.NAME);
     }
   
-    @Override
+   
     public String getBranch() {
         return this.getParameter(BRANCH);
     }
 
-    @Override
+  
     public String getCallId() {
        return callId;
     }
 
-    @Override
+   
     public String getMethod() {
         return this.getParameter(METHOD);
     }
 
-    @Override
+   
     public String getRel() {
         return this.getParameter(REL);
     }
 
-    @Override
+   
     public void setBranch(String branch) throws ParseException {
        this.setParameter(BRANCH,branch);
     }
 
-    @Override
+
     public void setCallId(String callId) {
         this.callId = callId;
     }
 
-    @Override
+   
     public void setMethod(String method) throws ParseException {
-        this.setParameter(METHOD,method);
+        if ( method != null ) {
+            this.setParameter(METHOD,method);
+        }
     }
 
-    @Override
+    
     public void setRel(String rel) throws ParseException{
-      this.setParameter(REL,rel);
+      if ( rel != null ) {
+          this.setParameter(REL,rel);
+      }
     }
 
-    @Override
+  
     public String getParameter(String name) {
         return super.getParameter(name);
     }
 
-    @Override
+    
     public Iterator getParameterNames() {
         return super.getParameterNames();
     }
 
-    @Override
+   
     public void removeParameter(String name) {
        super.removeParameter(name);
     }
 
-    @Override
+    
     public void setParameter(String name, String value) throws ParseException {
        super.setParameter(name,value); 
     }
 
-    @Override
+ 
     public String getName() {
         return ReferencesHeader.NAME;
     }
 
-    @Override
+   
     protected String encodeBody() {
         if ( super.parameters.isEmpty()) {
             return callId ;
@@ -92,7 +96,7 @@ public class References extends ParametersHeader  implements ReferencesHeader,Ex
         }
     }
 
-    @Override
+   
     public void setValue(String value) throws ParseException {
         throw new UnsupportedOperationException("operation not supported");
     }
