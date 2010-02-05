86ef23e91c239bf5ee98a871820a9473ac7f8d53
M. Ranganathan <mranga@nist.gov>
2010-02-05 21:18:06 +0000

    Add server authentication utililities. Revamp example.

---
+++ src/examples/authorization/AccountManagerImpl.java
@@ -0,0 +1,16 @@
+package examples.authorization;
+
+import javax.sip.ClientTransaction;
+
+import gov.nist.javax.sip.clientauthutils.AccountManager;
+import gov.nist.javax.sip.clientauthutils.UserCredentials;
+
+public class AccountManagerImpl implements AccountManager {
+    
+
+    @Override
+    public UserCredentials getCredentials(ClientTransaction challengedTransaction, String realm) {
+       return new UserCredentialsImpl("auth","nist.gov","pass");
+    }
+
+}
