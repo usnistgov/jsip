c3c27ff2baa0c50ad5b2077884cbc9dfedcbae90
mranga <mranga@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2010-02-14 22:57:14 +0000

    Fix test case.

--- trunk/src/test/unit/gov/nist/javax/sip/stack/ReInviteBusyTest.java
+++ trunk/src/test/unit/gov/nist/javax/sip/stack/ReInviteBusyTest.java
@@ -26,7 +26,10 @@ package test.unit.gov.nist.javax.sip.stack;
 
 import java.util.ArrayList;
 import java.util.EventObject;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -36,15 +39,20 @@ import javax.sip.DialogState;
 import javax.sip.DialogTerminatedEvent;
 import javax.sip.IOExceptionEvent;
 import javax.sip.ListeningPoint;
+import javax.sip.ObjectInUseException;
 import javax.sip.RequestEvent;
 import javax.sip.ResponseEvent;
 import javax.sip.ServerTransaction;
+import javax.sip.SipException;
+import javax.sip.SipFactory;
 import javax.sip.SipListener;
 import javax.sip.SipProvider;
+import javax.sip.SipStack;
 import javax.sip.TimeoutEvent;
 import javax.sip.Transaction;
 import javax.sip.TransactionTerminatedEvent;
 import javax.sip.address.Address;
+import javax.sip.address.AddressFactory;
 import javax.sip.address.SipURI;
 import javax.sip.header.CSeqHeader;
 import javax.sip.header.CallIdHeader;
@@ -52,10 +60,12 @@ import javax.sip.header.ContactHeader;
 import javax.sip.header.ContentTypeHeader;
 import javax.sip.header.FromHeader;
 import javax.sip.header.Header;
+import javax.sip.header.HeaderFactory;
 import javax.sip.header.MaxForwardsHeader;
 import javax.sip.header.RouteHeader;
 import javax.sip.header.ToHeader;
 import javax.sip.header.ViaHeader;
+import javax.sip.message.MessageFactory;
 import javax.sip.message.Request;
 import javax.sip.message.Response;
 
@@ -67,6 +77,7 @@ import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.SimpleLayout;
 import org.apache.log4j.helpers.NullEnumeration;
 
+import test.tck.msgflow.callflows.NonSipUriRouter;
 import test.tck.msgflow.callflows.ProtocolObjects;
 import test.tck.msgflow.callflows.ScenarioHarness;
 
@@ -89,6 +100,8 @@ public class ReInviteBusyTest extends TestCase {
     protected static final Appender console = new ConsoleAppender(new SimpleLayout());
 
     protected static Logger logger = Logger.getLogger(ReInviteBusyTest.class);
+    
+    
 
     static {
 
@@ -101,6 +114,144 @@ public class ReInviteBusyTest extends TestCase {
     private static int PEER_PORT = Shootme.myPort;
 
     private static String peerHostPort = PEER_ADDRESS + ":" + PEER_PORT;
+    
+    
+   
+    /**
+     * @author M. Ranganathan
+     *
+     */
+    public class ProtocolObjects {
+        public final AddressFactory addressFactory;
+
+        public final MessageFactory messageFactory;
+
+        public final HeaderFactory headerFactory;
+
+        public final SipStack sipStack;
+
+        public int logLevel = 32;
+
+        String logFileDirectory = "logs/";
+
+        public final String transport;
+
+        private boolean isStarted;
+
+    	public boolean autoDialog;
+    	
+
+        public ProtocolObjects(String stackname, String pathname, String transport,
+                boolean autoDialog, boolean isBackToBackUserAgent) {
+
+        	this.autoDialog = autoDialog;
+            this.transport = transport;
+            SipFactory sipFactory = SipFactory.getInstance();
+            sipFactory.resetFactory();
+            sipFactory.setPathName(pathname);
+            Properties properties = new Properties();
+            properties.setProperty("javax.sip.STACK_NAME", stackname);
+
+            // The following properties are specific to nist-sip
+            // and are not necessarily part of any other jain-sip
+            // implementation.
+            properties.setProperty("gov.nist.javax.sip.DEBUG_LOG", logFileDirectory
+                    + stackname + "debuglog.txt");
+            properties.setProperty("gov.nist.javax.sip.SERVER_LOG",
+                    logFileDirectory + stackname + "log.txt");
+
+            properties.setProperty("javax.sip.AUTOMATIC_DIALOG_SUPPORT",
+                    (autoDialog ? "on" : "off"));
+
+            // For the forked subscribe notify test
+            properties.setProperty("javax.sip.FORKABLE_EVENTS", "foo");
+
+            //For the TelUrlRouter test.
+            //properties.setProperty("javax.sip.ROUTER_PATH", NonSipUriRouter.class.getName());
+
+            // Dont use the router for all requests.
+            properties.setProperty("javax.sip.USE_ROUTER_FOR_ALL_URIS", "false");
+
+
+            properties.setProperty("gov.nist.javax.sip.THREAD_POOL_SIZE", "1");
+            
+            properties.setProperty("gov.nist.javax.sip.IS_BACK_TO_BACK_USER_AGENT", Boolean.toString(isBackToBackUserAgent));
+            //For the TelUrlRouter test.
+            properties.setProperty("javax.sip.ROUTER_PATH", NonSipUriRouter.class.getName());
+
+
+            // Set to 0 in your production code for max speed.
+            // You need 16 for logging traces. 32 for debug + traces.
+            // Your code will limp at 32 but it is best for debugging.
+            properties.setProperty("gov.nist.javax.sip.TRACE_LEVEL", new Integer(
+                    logLevel).toString());
+
+            try {
+                // Create SipStack object
+                sipStack = sipFactory.createSipStack(properties);
+
+                NonSipUriRouter router = (NonSipUriRouter) sipStack.getRouter();
+
+                router.setMyPort(5080);
+
+                System.out.println("createSipStack " + sipStack);
+            } catch (Exception e) {
+                // could not find
+                // gov.nist.jain.protocol.ip.sip.SipStackImpl
+                // in the classpath
+                e.printStackTrace();
+                System.err.println(e.getMessage());
+                throw new RuntimeException("Stack failed to initialize");
+            }
+
+            try {
+                headerFactory = sipFactory.createHeaderFactory();
+                addressFactory = sipFactory.createAddressFactory();
+                messageFactory = sipFactory.createMessageFactory();
+            } catch (SipException ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex);
+            }
+        }
+
+        public synchronized void destroy() {
+
+            HashSet hashSet = new HashSet();
+
+            for (Iterator it = sipStack.getSipProviders(); it.hasNext();) {
+
+                SipProvider sipProvider = (SipProvider) it.next();
+                hashSet.add(sipProvider);
+            }
+
+            for ( Iterator it = hashSet.iterator(); it.hasNext();) {
+                SipProvider sipProvider = (SipProvider) it.next();
+
+                for (int j = 0; j < 5; j++) {
+                    try {
+                        sipStack.deleteSipProvider(sipProvider);
+                    } catch (ObjectInUseException ex) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                        }
+
+                    }
+                }
+            }
+
+            sipStack.stop();
+        }
+
+        public void start() throws Exception {
+            if (this.isStarted)
+                return;
+            sipStack.start();
+            this.isStarted = true;
+
+        }
+    }
+
 
     public class Shootme implements SipListener {
 
@@ -145,9 +296,6 @@ public class ReInviteBusyTest extends TestCase {
 
         }
 
-        /**
-         * Process the ACK request. Send the bye and complete the call flow.
-         */
         public void processAck(RequestEvent requestEvent, ServerTransaction serverTransaction) {
             SipProvider sipProvider = (SipProvider) requestEvent.getSource();
             try {
@@ -158,6 +306,7 @@ public class ReInviteBusyTest extends TestCase {
                 dialog = inviteTid.getDialog();
                 Thread.sleep(1000);
                 logger.info("shootme is sending RE INVITE now");
+                System.out.println("Got an ACK " );
                 this.reInviteCount++;
                 this.sendReInvite(sipProvider);
 
@@ -175,7 +324,6 @@ public class ReInviteBusyTest extends TestCase {
             SipProvider sipProvider = (SipProvider) requestEvent.getSource();
             Request request = requestEvent.getRequest();
              try {
-                // logger.info("shootme: " + request);
                 ServerTransaction st = requestEvent.getServerTransaction();
                 int finalResponse;
                 logger.info("Got an INVITE  " + request + " serverTx = " + st);
@@ -184,14 +332,17 @@ public class ReInviteBusyTest extends TestCase {
                     st = sipProvider.getNewServerTransaction(request);
                     logger.info("Server transaction created!" + request);
 
-                    logger.info("Dialog = " + st.getDialog());
+                    System.out.println("Dialog = " + st.getDialog());
                     if (st.getDialog().getApplicationData() == null) {
                         st.getDialog().setApplicationData(new ApplicationData());
                     }
+                 
                     finalResponse = 200;
                 } else {
                     // If Server transaction is not null, then
                     // this is a re-invite.
+                	System.out.println("Dialog = " + st.getDialog());
+                       
                     logger.info("This is a RE INVITE ");
                     this.reInviteCount++;
                     ReInviteBusyTest.assertSame("Dialog mismatch ", st.getDialog(), this.dialog);
