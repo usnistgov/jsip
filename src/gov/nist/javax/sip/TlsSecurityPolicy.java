f383e4eb92212373f6372c0a8d8dd29707c6f329
cbeeton <cbeeton@users.noreply.github.com>
2010-02-22 18:57:34 +0000

    Change enforceTlsPolicy to throw SecurityException.  Minor cleanup. Issue number:  268 Reviewed by:   mranga

--- src/gov/nist/javax/sip/TlsSecurityPolicy.java
+++ src/gov/nist/javax/sip/TlsSecurityPolicy.java
@@ -1,9 +1,5 @@
 package gov.nist.javax.sip;
 
-import java.io.IOException;
-
-import javax.sip.ClientTransaction;
-
 /**
  * An implementation of this interface can be registered with the sip stack using the 
  * configuration property gov.nist.javax.sip.TLS_SECURITY_POLICY
@@ -14,8 +10,8 @@ public interface TlsSecurityPolicy {
      * Enforce any application-specific security policy for TLS clients.
      * Called when establishing an outgoing TLS connection.
      * @param transaction -- the transaction context for the connection
-     * @throws IOException -- if the certificates extracted from the client transacton are not acceptable.
+     * @throws SecurityException -- if the certificates extracted from the client transaction are not acceptable.
      */
-    void enforceTlsPolicy(ClientTransactionExt transaction) throws IOException;
+    void enforceTlsPolicy(ClientTransactionExt transaction) throws SecurityException;
 
 }
