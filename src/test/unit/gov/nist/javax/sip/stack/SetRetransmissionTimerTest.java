c313388aeb3f0929f56b3eed79ae8f62cef4024f
Jean Deruelle <jean.deruelle@gmail.com>
2010-07-09 12:46:42 +0000

    Fix failling test
    Issue number:  303
    Obtained from:
    Submitted by: Jean Deruelle
    Reviewed by:

--- src/test/unit/gov/nist/javax/sip/stack/SetRetransmissionTimerTest.java
+++ src/test/unit/gov/nist/javax/sip/stack/SetRetransmissionTimerTest.java
@@ -389,7 +389,7 @@ public class SetRetransmissionTimerTest extends TestCase {
                 // Create the client transaction.
                 inviteTid = sipProvider.getNewClientTransaction(request);
                 inviteTid.setRetransmitTimer(100);
-                ((TransactionExt)inviteTid).setTimerD(6400);
+                ((TransactionExt)inviteTid).setTimerD(64000);
                 ((TransactionExt)inviteTid).setTimerT2(800);
                 ((TransactionExt)inviteTid).setTimerT4(1000);
                 // send the request out.
@@ -536,7 +536,7 @@ public class SetRetransmissionTimerTest extends TestCase {
                 if (st == null) {
                     st = sipProvider.getNewServerTransaction(request);
                     st.setRetransmitTimer(100);
-                    ((TransactionExt)st).setTimerD(6400);
+                    ((TransactionExt)st).setTimerD(64000);
                     ((TransactionExt)st).setTimerT2(800);
                     ((TransactionExt)st).setTimerT4(1000);
                 }
