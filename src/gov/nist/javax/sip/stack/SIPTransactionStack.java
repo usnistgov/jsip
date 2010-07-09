f8535c183076a71361cb30c32e4a116ed630b1b0
vralev <vladimirralev@users.noreply.github.com>
2010-07-09 10:31:15 +0000

    Issue number: 297 Obtained from: vralev Submitted by: vralev Reviewed by:  jean
    Removing @Override from anon classes. JDK 1.5 problem

--- src/gov/nist/javax/sip/stack/SIPTransactionStack.java
+++ src/gov/nist/javax/sip/stack/SIPTransactionStack.java
@@ -101,7 +101,7 @@ import javax.sip.message.Response;
  * 
  * @author M. Ranganathan <br/>
  * 
- * @version 1.2 $Revision: 1.152.2.2 $ $Date: 2010-07-08 14:52:55 $
+ * @version 1.2 $Revision: 1.152.2.3 $ $Date: 2010-07-09 10:31:15 $
  */
 public abstract class SIPTransactionStack implements
 		SIPTransactionEventListener, SIPDialogEventListener {
@@ -408,7 +408,6 @@ public abstract class SIPTransactionStack implements
 	
 	private static class SameThreadExecutor implements Executor {
 
-		@Override
 		public void execute(Runnable command) {
 			command.run(); // Just run the command is the same thread
 		}
