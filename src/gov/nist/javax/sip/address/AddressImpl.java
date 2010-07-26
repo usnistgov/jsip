38b177c59bc1fce5ba84cefd755fb22610f2e263
Jean Deruelle <jean.deruelle@gmail.com>
2010-07-26 15:26:13 +0000

    Don't add the angle brackets if already present in the sipuri
    Issue number:  315
    Obtained from:
    Submitted by: Jean Deruelle
    Reviewed by:

--- src/gov/nist/javax/sip/address/AddressImpl.java
+++ src/gov/nist/javax/sip/address/AddressImpl.java
@@ -40,7 +40,7 @@ import javax.sip.address.*;
  *
  *
  *
- *@version 1.2 $Revision: 1.12 $ $Date: 2010-05-06 14:08:05 $
+ *@version 1.2 $Revision: 1.13 $ $Date: 2010-07-26 15:26:13 $
  *
  */
 public final class AddressImpl
@@ -176,10 +176,17 @@ public final class AddressImpl
                         .append(SP);
             }
             if (address != null) {
-                if (addressType == NAME_ADDR || displayName != null)
+                String uriString = address.encode().trim();
+                // Issue 316 : createAddress can add spurious angle brackets
+                // avoid adding brackets if already present
+                boolean containsAngleBrackets = false;
+                if(uriString.startsWith(LESS_THAN) && uriString.endsWith(GREATER_THAN)) {
+                    containsAngleBrackets = true;
+                }
+                if (!containsAngleBrackets && (addressType == NAME_ADDR || displayName != null))
                     buffer.append(LESS_THAN);
                 address.encode(buffer);
-                if (addressType == NAME_ADDR || displayName != null)
+                if (!containsAngleBrackets && (addressType == NAME_ADDR || displayName != null))
                     buffer.append(GREATER_THAN);
             }
         }
