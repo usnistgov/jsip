2b317918992e54e8c92b961ddf6eaf73e6c21b1c
Jean Deruelle <jean.deruelle@gmail.com>
2010-08-02 16:44:48 +0000

    Fixing properly forking when flag is not set. Creating a forked dialog only upong the correct conditions
    Issue number:  317
    Obtained from:
    Submitted by:  Jean Deruelle
    Reviewed by:

--- src/test/unit/gov/nist/javax/sip/stack/forkedinvite/InviteTest.java
+++ src/test/unit/gov/nist/javax/sip/stack/forkedinvite/InviteTest.java
@@ -27,7 +27,7 @@ public class InviteTest extends TestCase {
 
     protected static final Appender console = new ConsoleAppender(new SimpleLayout());
 
-    private static final int forkCount = 2;
+    private static int forkCount = 2;
     
    
 
@@ -49,6 +49,8 @@ public class InviteTest extends TestCase {
 
         try {
             super.setUp();
+            
+
         } catch (Exception ex) {
             fail("unexpected exception ");
         }
@@ -56,33 +58,7 @@ public class InviteTest extends TestCase {
 
     public void tearDown() {
         try {
-            Thread.sleep(12000);
-           
             
-            this.shootist.checkState();
-            
-            int ackCount = 0;
-           
-            for ( Shootme shootme: this.shootme) {
-                 shootme.checkState();
-                
-                 
-                 if ( shootme.isAckSeen()) {
-                     ackCount ++;
-                 }
-            }
-           
-           assertEquals("ACK count must be exactly 2", 2,ackCount);
-            
-            this.shootist.stop();
-            
-            for ( Shootme shootme: this.shootme) {
-              
-                shootme.stop();
-           }
-
-        
-            this.proxy.stop();
         } catch (Exception ex) {
             ex.printStackTrace();
             logger.error("unexpected exception", ex);
@@ -91,31 +67,7 @@ public class InviteTest extends TestCase {
     }
 
     public void testInvite() throws Exception {
-    	shootist = new Shootist(6050, 5070, "on");
-        SipProvider shootistProvider = shootist.createSipProvider();
-        shootistProvider.addSipListener(shootist);
-        boolean sendRinging = true;
-        for  (int i = 0 ; i <  forkCount ; i ++ ) {
-            
-            Shootme shootme = new Shootme(5080 + i,sendRinging,400*forkCount - 200*i);
-            sendRinging = false;
-            SipProvider shootmeProvider = shootme.createProvider();
-            shootmeProvider.addSipListener(shootme);
-            this.shootme.add(shootme);
-        }
-
-       
-
-        this.proxy = new Proxy(5070,forkCount);
-        SipProvider provider = proxy.createSipProvider();
-        provider.addSipListener(proxy);
-        logger.debug("setup completed");
-        this.shootist.sendInvite(forkCount);
-
-    }
-    
-    public void testInviteAutomaticDialogNonEnabled() throws Exception {
-        shootist = new Shootist(6050, 5070, "off");        
+        shootist = new Shootist(6050, 5070, "on");
         SipProvider shootistProvider = shootist.createSipProvider();
         shootistProvider.addSipListener(shootist);
         boolean sendRinging = true;
@@ -136,8 +88,98 @@ public class InviteTest extends TestCase {
         logger.debug("setup completed");
         
         this.shootist.sendInvite(forkCount);
-
+        
+        Thread.sleep(12000);
+        this.shootist.checkState();
+        int ackCount = 0;
+        for ( Shootme shootme: this.shootme) {
+             shootme.checkState();
+             if ( shootme.isAckSeen()) {
+                 ackCount ++;
+             }
+        }
+        assertEquals("ACK count must be exactly 2", 2,ackCount);
+        this.shootist.stop();
+        for ( Shootme shootme: this.shootme) {
+            shootme.stop();
+        }
+        this.proxy.stop();
     }
 
+    public void testInviteAutomaticDialogNonEnabled() throws Exception {
+        shootist = new Shootist(6050, 5070, "off");        
+        SipProvider shootistProvider = shootist.createSipProvider();
+        shootistProvider.addSipListener(shootist);
+        boolean sendRinging = true;
+        for  (int i = 0 ; i <  forkCount ; i ++ ) {
+            Shootme shootme = new Shootme(5080 + i,sendRinging,400*forkCount - 200*i);
+            sendRinging = false;
+            SipProvider shootmeProvider = shootme.createProvider();
+            shootmeProvider.addSipListener(shootme);
+            this.shootme.add(shootme);
+        }
+        this.proxy = new Proxy(5070,forkCount);
+        SipProvider provider = proxy.createSipProvider();
+        provider.addSipListener(proxy);
+        logger.debug("setup completed");
+        
+        this.shootist.sendInvite(forkCount);
+        Thread.sleep(12000);
+        this.shootist.checkState();
+        int ackCount = 0;
+        for ( Shootme shootme: this.shootme) {
+             shootme.checkState();
+             if ( shootme.isAckSeen()) {
+                 ackCount ++;
+             }
+        }
+        assertEquals("ACK count must be exactly 2", 2,ackCount);
+        this.shootist.stop();
+        for ( Shootme shootme: this.shootme) {
+            shootme.stop();
+        }
+        this.proxy.stop();
+    }
+    
+    /**
+     * Checking if when the flag is not enabled and a 200 ok response comes before
+     * the app code has called createNewDialog doesn't create a dialog 
+     */
+    public void testAutomaticDialogNonEnabledRaceCondition() throws Exception {
+        shootist = new Shootist(6050, 5070, "off"); 
+        shootist.setCreateDialogAfterRequest(true);
+        SipProvider shootistProvider = shootist.createSipProvider();
+        shootistProvider.addSipListener(shootist);
+        boolean sendRinging = true;
+        forkCount = 1;
+        for  (int i = 0 ; i <  forkCount ; i ++ ) {
+            Shootme shootme = new Shootme(5080 + i,sendRinging,400*forkCount - 200*i);
+            sendRinging = false;
+            SipProvider shootmeProvider = shootme.createProvider();
+            shootmeProvider.addSipListener(shootme);
+            this.shootme.add(shootme);
+        }
+        this.proxy = new Proxy(5070,forkCount);
+        SipProvider provider = proxy.createSipProvider();
+        provider.addSipListener(proxy);
+        logger.debug("setup completed");
+        
+        this.shootist.sendInvite(0);
+        Thread.sleep(12000);
+        this.shootist.checkState();
+        int ackCount = 0;
+        for ( Shootme shootme: this.shootme) {
+             if ( shootme.isAckSeen()) {
+                 ackCount ++;
+             }
+        }
+        assertEquals("ACK count must be exactly 0", 0,ackCount);
+        this.shootist.stop();
+        for ( Shootme shootme: this.shootme) {
+            shootme.stop();
+        }
+        this.proxy.stop();
+    }
 
+    
 }
