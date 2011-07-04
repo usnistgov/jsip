52d9787731cdaa4543609bcec5937711c70dff03
deruelle_jean <deruelle_jean@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2011-07-04 15:16:15 +0000

    http://java.net/jira/browse/JSIP-377 caused a regression see http://code.google.com/p/mobicents/issues/detail?id=2616, so we should only pass up 2xx for proxies

--- trunk/src/gov/nist/javax/sip/stack/SIPClientTransaction.java
+++ trunk/src/gov/nist/javax/sip/stack/SIPClientTransaction.java
@@ -804,21 +804,22 @@ public class SIPClientTransaction extends SIPTransaction implements ServerRespon
                 } catch (SipException ex) {
                     // What to do here ?? kill the dialog?
                 }
-            }
-            if(dialog == null) {
+            }            
+            if(dialog == null && statusCode >= 200 && statusCode < 300) {
             	// http://java.net/jira/browse/JSIP-377
+            	// RFC 3261 Section 17.1.1.2
             	// The client transaction MUST be destroyed the instant it enters the
             	// "Terminated" state.  This is actually necessary to guarantee correct
             	// operation.  The reason is that 2xx responses to an INVITE are treated
             	// differently; each one is forwarded by proxies
             	
                 // for proxy, it happens that there is a race condition while the tx is getting removed and TERMINATED
-            	// where some responses are still able to be handled by it so we let responses for proxies pass up to the application
+            	// where some responses are still able to be handled by it so we let 2xx responses for proxies pass up to the application
             	if (logger.isLoggingEnabled(LogWriter.TRACE_DEBUG))
                     logger.logDebug("Client Transaction " + this + " branch id " + getBranch() + " doesn't have any dialog and is in TERMINATED state");
             	if (respondTo != null) {
             		if (logger.isLoggingEnabled(LogWriter.TRACE_DEBUG))
-                        logger.logDebug("passing response up to the application");
+                        logger.logDebug("passing 2xx response up to the application");
                     respondTo.processResponse(transactionResponse, this, dialog);
                 } else {
                     this.semRelease();
