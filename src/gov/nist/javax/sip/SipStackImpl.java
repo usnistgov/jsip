cfa32e42a4df27de7ef3ab0b44f9f264222dd40e
Jean Deruelle <jean.deruelle@gmail.com>
2012-09-18 11:11:29 +0000

    Update Issue 18 (git repo)
    JSIP-416
    
    Better exception handling when there is no CallID on a message received over NIO+ removal of USE_NIO property that wasn't used

--- src/gov/nist/javax/sip/SipStackImpl.java
+++ src/gov/nist/javax/sip/SipStackImpl.java
@@ -1411,8 +1411,6 @@ public class SipStackImpl extends SIPTransactionStack implements
 			}
 		}
 		
-		super.useNio = Boolean.parseBoolean( configurationProperties.getProperty("gov.nist.javax.sip.USE_NIO","true"));
-		
 	}
 
 	/*
