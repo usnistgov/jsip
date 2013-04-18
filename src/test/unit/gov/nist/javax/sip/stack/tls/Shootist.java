3100765412e4e628731a8cad72706812363dbdc3
Jean Deruelle <jean.deruelle@gmail.com>
2013-04-18 15:09:05 +0000

    Update Issue 93
    Issue 451
    JAIN SIP Repo
    
    Fix + non regression tests

--- src/test/unit/gov/nist/javax/sip/stack/tls/Shootist.java
+++ src/test/unit/gov/nist/javax/sip/stack/tls/Shootist.java
@@ -3,6 +3,7 @@ import gov.nist.javax.sip.ClientTransactionExt;
 import gov.nist.javax.sip.TlsSecurityPolicy;
 import gov.nist.javax.sip.header.HeaderExt;
 import gov.nist.javax.sip.stack.NioMessageProcessorFactory;
+import gov.nist.javax.sip.stack.SIPTransactionStack;
 
 import java.security.cert.Certificate;
 import java.util.ArrayList;
@@ -179,52 +180,62 @@ public class Shootist implements SipListener, TlsSecurityPolicy {
         System.out.println("Transaction Time out" );
     }
 
+    
     public void init(String domain) {
+        init(domain, null);
+    }
+    
+    public void init(String domain, Properties props) {
     	this.domain = domain;
         SipFactory sipFactory = null;
         sipStack = null;
         sipFactory = SipFactory.getInstance();
         sipFactory.setPathName("gov.nist");
-        Properties properties = new Properties();
+       
         String transport = "tls";
         int port = 5061;
         String peerHostPort = "127.0.0.1:5071";
-        properties.setProperty(
-            "javax.sip.OUTBOUND_PROXY",
-            peerHostPort + "/" + transport);
-        // If you want to use UDP then uncomment this.
-        //properties.setProperty(
-        //  "javax.sip.ROUTER_PATH",
-        //  "examples.shootistTLS.MyRouter");
-        properties.setProperty("javax.sip.STACK_NAME", "shootist");
-   
-        // The following properties are specific to nist-sip
-        // and are not necessarily part of any other jain-sip
-        // implementation.
-        // You can set a max message size for tcp transport to
-        // guard against denial of service attack.
-        properties.setProperty("gov.nist.javax.sip.MAX_MESSAGE_SIZE",
-                    "1048576");
-        properties.setProperty(
-            "gov.nist.javax.sip.DEBUG_LOG",
-            "logs/shootistdebug.txt");
-        properties.setProperty(
-            "gov.nist.javax.sip.SERVER_LOG",
-            "logs/shootistlog.txt");
-        properties.setProperty(
-                "gov.nist.javax.sip.SSL_HANDSHAKE_TIMEOUT", "10000");
-        properties.setProperty("gov.nist.javax.sip.TCP_POST_PARSING_THREAD_POOL_SIZE", "20");
-        properties.setProperty("gov.nist.javax.sip.TLS_SECURITY_POLICY",
-                this.getClass().getName());
-
-        // Drop the client connection after we are done with the transaction.
-        properties.setProperty("gov.nist.javax.sip.CACHE_CLIENT_CONNECTIONS", "false");
-        // Set to 0 in your production code for max speed.
-        // You need  16 for logging traces. 32 for debug + traces.
-        // Your code will limp at 32 but it is best for debugging.
-        properties.setProperty("gov.nist.javax.sip.TRACE_LEVEL", "LOG4J");
-        if(System.getProperty("enableNIO") != null && System.getProperty("enableNIO").equalsIgnoreCase("true")) {
-        	properties.setProperty("gov.nist.javax.sip.MESSAGE_PROCESSOR_FACTORY", NioMessageProcessorFactory.class.getName());
+        Properties properties = new Properties(); 
+        if(props == null) {                   
+            properties.setProperty(
+                "javax.sip.OUTBOUND_PROXY",
+                peerHostPort + "/" + transport);
+            // If you want to use UDP then uncomment this.
+            //properties.setProperty(
+            //  "javax.sip.ROUTER_PATH",
+            //  "examples.shootistTLS.MyRouter");
+            properties.setProperty("javax.sip.STACK_NAME", "shootist");
+       
+            // The following properties are specific to nist-sip
+            // and are not necessarily part of any other jain-sip
+            // implementation.
+            // You can set a max message size for tcp transport to
+            // guard against denial of service attack.
+            properties.setProperty("gov.nist.javax.sip.MAX_MESSAGE_SIZE",
+                        "1048576");
+            properties.setProperty(
+                "gov.nist.javax.sip.DEBUG_LOG",
+                "logs/shootistdebug.txt");
+            properties.setProperty(
+                "gov.nist.javax.sip.SERVER_LOG",
+                "logs/shootistlog.txt");
+            properties.setProperty(
+                    "gov.nist.javax.sip.SSL_HANDSHAKE_TIMEOUT", "10000");
+            properties.setProperty("gov.nist.javax.sip.TCP_POST_PARSING_THREAD_POOL_SIZE", "20");
+            properties.setProperty("gov.nist.javax.sip.TLS_SECURITY_POLICY",
+                    this.getClass().getName());
+    
+            // Drop the client connection after we are done with the transaction.
+            properties.setProperty("gov.nist.javax.sip.CACHE_CLIENT_CONNECTIONS", "false");
+            // Set to 0 in your production code for max speed.
+            // You need  16 for logging traces. 32 for debug + traces.
+            // Your code will limp at 32 but it is best for debugging.
+            properties.setProperty("gov.nist.javax.sip.TRACE_LEVEL", "LOG4J");
+            if(System.getProperty("enableNIO") != null && System.getProperty("enableNIO").equalsIgnoreCase("true")) {
+            	properties.setProperty("gov.nist.javax.sip.MESSAGE_PROCESSOR_FACTORY", NioMessageProcessorFactory.class.getName());
+            }
+        } else {
+            properties = props;
         }
         try {
             // Create SipStack object
@@ -398,17 +409,19 @@ public class Shootist implements SipListener, TlsSecurityPolicy {
             listener.inviteTid.sendRequest();
             
             System.out.println("isSecure = " + ((ClientTransactionExt)listener.inviteTid).isSecure());
-            if ( ((ClientTransactionExt)listener.inviteTid).isSecure() ) {
-                System.out.println("cipherSuite = " + ((ClientTransactionExt)listener.inviteTid).getCipherSuite());
-                if(((ClientTransactionExt)listener.inviteTid).getLocalCertificates() != null) {
-	                for ( Certificate cert : ((ClientTransactionExt)listener.inviteTid).getLocalCertificates()) {
-	                    System.out.println("localCert =" + cert);
-	                }
-                }
-                if(((ClientTransactionExt)listener.inviteTid).getPeerCertificates() != null) {
-	                for ( Certificate cert : ((ClientTransactionExt)listener.inviteTid).getPeerCertificates()) {
-	                    System.out.println("remoteCerts = " + cert);
-	                }
+            if(!((SIPTransactionStack)sipStack).getMessageProcessorFactory().getClass().getName().equals(NioMessageProcessorFactory.class.getName())) {
+                if ( ((ClientTransactionExt)listener.inviteTid).isSecure() ) {
+                    System.out.println("cipherSuite = " + ((ClientTransactionExt)listener.inviteTid).getCipherSuite());
+                    if(((ClientTransactionExt)listener.inviteTid).getLocalCertificates() != null) {
+    	                for ( Certificate cert : ((ClientTransactionExt)listener.inviteTid).getLocalCertificates()) {
+    	                    System.out.println("localCert =" + cert);
+    	                }
+                    }
+                    if(((ClientTransactionExt)listener.inviteTid).getPeerCertificates() != null) {
+    	                for ( Certificate cert : ((ClientTransactionExt)listener.inviteTid).getPeerCertificates()) {
+    	                    System.out.println("remoteCerts = " + cert);
+    	                }
+                    }
                 }
             }
         } catch (Exception ex) {
--- src/test/unit/gov/nist/javax/sip/stack/tls/TlsTest.java
+++ src/test/unit/gov/nist/javax/sip/stack/tls/TlsTest.java
@@ -1,11 +1,10 @@
 package test.unit.gov.nist.javax.sip.stack.tls;
 
-import junit.framework.TestCase;
+import gov.nist.javax.sip.stack.NioMessageProcessorFactory;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
+import java.util.Properties;
+
+import junit.framework.TestCase;
 
 public class TlsTest extends TestCase {
 	
@@ -19,10 +18,51 @@ public class TlsTest extends TestCase {
         System.setProperty( "javax.net.ssl.trustStore", TlsTest.class.getResource("testkeys").getPath() );
         System.setProperty( "javax.net.ssl.keyStorePassword", "passphrase" );
         System.setProperty( "javax.net.ssl.keyStoreType", "jks" );
-        this.shootist = new Shootist();
-		this.shootme = new Shootme();
-		this.shootme.init();
+//        this.shootist = new Shootist();
+//		this.shootme = new Shootme();
+//		this.shootme.init();
 	}
+
+	public void testNIOTlsProtocols() {        
+        this.shootme = new Shootme();
+        this.shootme.init();
+        this.shootist = new Shootist();
+        Properties properties = new Properties();        
+        // If you want to use UDP then uncomment this.
+        //properties.setProperty(
+        //  "javax.sip.ROUTER_PATH",
+        //  "examples.shootistTLS.MyRouter");
+        properties.setProperty("javax.sip.STACK_NAME", "shootist");
+   
+        // The following properties are specific to nist-sip
+        // and are not necessarily part of any other jain-sip
+        // implementation.
+        // You can set a max message size for tcp transport to
+        // guard against denial of service attack.
+        properties.setProperty("gov.nist.javax.sip.MAX_MESSAGE_SIZE",
+                    "1048576");
+        properties.setProperty(
+            "gov.nist.javax.sip.DEBUG_LOG",
+            "logs/shootistdebug.txt");
+        properties.setProperty(
+            "gov.nist.javax.sip.SERVER_LOG",
+            "logs/shootistlog.txt");
+        properties.setProperty(
+                "gov.nist.javax.sip.SSL_HANDSHAKE_TIMEOUT", "10000");
+        properties.setProperty("gov.nist.javax.sip.TCP_POST_PARSING_THREAD_POOL_SIZE", "20");
+        properties.setProperty("gov.nist.javax.sip.TLS_SECURITY_POLICY",
+                Shootist.class.getName());
+
+        // Drop the client connection after we are done with the transaction.
+        properties.setProperty("gov.nist.javax.sip.CACHE_CLIENT_CONNECTIONS", "false");
+        // Set to 0 in your production code for max speed.
+        // You need  16 for logging traces. 32 for debug + traces.
+        // Your code will limp at 32 but it is best for debugging.
+        properties.setProperty("gov.nist.javax.sip.TRACE_LEVEL", "32");
+        properties.setProperty("gov.nist.javax.sip.MESSAGE_PROCESSOR_FACTORY", NioMessageProcessorFactory.class.getName());
+        properties.setProperty("gov.nist.javax.sip.TLS_CLIENT_PROTOCOLS", "SSLv2Hello, TLSv1");
+        this.shootist.init("localhost", properties);
+    }
 	
 	public void testTls() {
         this.shootist = new Shootist();
@@ -31,8 +71,8 @@ public class TlsTest extends TestCase {
 		this.shootist.init("localhost");
 	}
 	
-	public void tearDown() {
-		try {
+	@Override
+	public void tearDown() throws Exception {
             Thread.sleep(2000);
             if(this.shootme != null)
             	this.shootme.stop();
@@ -46,8 +86,5 @@ public class TlsTest extends TestCase {
             System.clearProperty( "javax.net.ssl.keyStoreType" );
 
            
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
     }
 }
