92faf98dd92387345835095f3da53ae325246e6e
deruelle_jean <deruelle_jean@8e71dc83-d81e-6649-80f2-80b843a9b2be>
2014-08-19 16:52:21 +0000

    Update Issue 143
    Adding new method to allow creating a challenge request with pure looserouting

--- trunk/src/gov/nist/javax/sip/clientauthutils/AuthenticationHelper.java
+++ trunk/src/gov/nist/javax/sip/clientauthutils/AuthenticationHelper.java
@@ -58,6 +58,39 @@ public interface AuthenticationHelper {
              NullPointerException;
 
     /**
+     * Uses securityAuthority to determinie a set of valid user credentials for
+     * the specified Response (Challenge) and appends it to the challenged
+     * request so that it could be retransmitted.
+     *
+     *
+     *
+     * @param challenge
+     *            the 401/407 challenge response
+     * @param challengedTransaction
+     *            the transaction established by the challenged request
+     * @param transactionCreator
+     *            the JAIN SipProvider that we should use to create the new
+     *            transaction.
+     * @param cacheTime The amount of time (seconds ) for which the authentication helper
+     *      will keep a reference to the generated credentials in a cache.
+     *      If you specify -1, then the authentication credentials are cached
+     *      until you remove them from the cache. If you choose this option, make sure
+     *      you remove the cached headers or you will have a memory leak.
+     * @param useLooseRouting avoid setting maddr and port on the request uri before calculating the response
+     *
+     * @return a transaction containing a re-originated request with the
+     *         necessary authorization header.
+     * @throws SipException
+     *             if we get an exception white creating the new transaction
+     * @throws NullPointerException
+     *             if an argument or a header is null.
+     */
+    public abstract ClientTransaction handleChallenge(Response challenge,
+            ClientTransaction challengedTransaction,
+            SipProvider transactionCreator, int cacheTime, boolean useLooseRouting ) throws SipException,
+             NullPointerException;
+    
+    /**
      * Attach authentication headers to the given request. This looks up
      * the credential cache and picks up any stored authentication headers
      * for the given call ID and attaches it to the request.
